242dce134057e53608cb82927418b144c1e255f9
Jonathan Tien <jonathan.tien@gmail.com>
2013-05-21 19:21:42 -0700

    initial schedule model

--- ap/schedules/models.py
+++ ap/schedules/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
-# Create your models here.
+########################################################################80chars
+
+"""
+SCHEDULES models.py
+
+This schedules module is for representing weekly trainee schedules.
+
+Data Models
+- Event: 
+    an event, such as class or study time, that trainees need to attend.
+- Schedule: 
+    a collection of events for one trainee. each trainee should have one 
+    schedule per term. 
+- ScheduleTemplate:
+    a generic collection of events for one week that can be applied to a 
+    trainee or group of trainees. 
+"""
+
+class Event(models.Model):
+    group = models.IntegerField(unique=True)
+    
+
+class Schedule(models.Model):
+
+class ScheduleTemplate(models.Model):
+
