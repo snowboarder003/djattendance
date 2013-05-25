2e99340840f8ae4107ac260b9d39bf77bac358f6
Jonathan Tien <jonathan.tien@gmail.com>
2013-05-25 16:09:32 -0700

    initial data model for term

--- ap/terms/models.py
+++ ap/terms/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
-# Create your models here.
+########################################################################80chars
+
+""" TERM models.py
+
+This schedules module is for representing weekly trainee schedules.
+
+Data Models
+- Term:
+	a term of the full-time training, consisting of twenty weeks
+
+"""
+
+class Term(models.Model):
+
+	# a term's long name; i.e. Fall 2013, Spring 2015
+	name = models.CharField(max_length=12)
+
+	# a term's short code; i.e. Fa13, Sp15
+	code = models.CharField(max_length=4)
+
+	# first day of the term, the monday of pre-training
+	start = models.DateField()
+
+	# the last day of the term, the sat of semiannual
+	end = models.DateField()
+
+
