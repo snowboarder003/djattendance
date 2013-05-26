4f29efb8f62c5e843220c26be1a8c63d54b242db
Jonathan Tien <jonathan.tien@gmail.com>
2013-05-25 19:10:30 -0700

    resolved lint issues, converted tabs to spaces

--- ap/terms/models.py
+++ ap/terms/models.py
@@ -8,22 +8,21 @@ This schedules module is for representing weekly trainee schedules.
 
 Data Models
 - Term:
-	a term of the full-time training, consisting of twenty weeks
+    a term of the full-time training, consisting of twenty weeks
 
 """
 
+
 class Term(models.Model):
 
-	# a term's long name; i.e. Fall 2013, Spring 2015
-	name = models.CharField(max_length=12)
+    # a term's long name; i.e. Fall 2013, Spring 2015
+    name = models.CharField(max_length=12)
 
-	# a term's short code; i.e. Fa13, Sp15
-	code = models.CharField(max_length=4)
+    # a term's short code; i.e. Fa13, Sp15
+    code = models.CharField(max_length=4)
 
-	# first day of the term, the monday of pre-training
-	start = models.DateField()
+    # first day of the term, the monday of pre-training
+    start = models.DateField()
 
-	# the last day of the term, the sat of semiannual
-	end = models.DateField()
-
-
+    # the last day of the term, the sat of semiannual
+    end = models.DateField()
