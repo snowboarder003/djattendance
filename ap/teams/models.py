697e6bc4ee36b79119e117d53c28aa282a9355fb
Jonathan Tien <jonathan.tien@gmail.com>
2013-05-29 19:12:06 -0700

    initial draft of teams model

--- ap/teams/models.py
+++ ap/teams/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
-# Create your models here.
+""" TEAMS app
+
+The teams app is responsible for representing teams in data. This is a utility
+class used by many other applications
+
+Data Models:
+    Team: an FTTA team
+"""
+
+class Team(Models.model):
+
+    TEAM_TYPES = (
+        ('CAMPUS', 'Campus')
+        ('CHILD', 'Children')
+        ('COM', 'Community')
+        ('YP', 'Young People')
+        ('I', 'Internet')
+    )
+
+    # the full name of a team, e.g. Irvine Young People, or Anaheim Community
+    name = models.CharField(max_length=50)
+
+    # the abbreviation of the team, e.g. I-YP or ANA-COM
+    shortcode = models.CharField(max_length=10)
+
+    type = models.CharField(max_length=6, choices=TEAM_TYPES)
