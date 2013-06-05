6a780a271ab0236607cc1d030b1fc5f08e19a94b
Jonathan Tien <jonathan.tien@gmail.com>
2013-06-04 17:10:09 -0700

    initial model definied. houses and bunks

--- ap/houses/models.py
+++ ap/houses/models.py
@@ -1,3 +1,52 @@
 from django.db import models
 
-# Create your models here.
+""" HOUSES models.py
+
+This houses module is a utility model that define training housing.
+
+Data Models:
+- House: a training house
+
+- Bunk: a bunk (either lower of upper) in a given house
+
+"""
+
+
+class House(models.Model):
+
+    GENDER = (
+        ('B', 'Brother'),
+        ('S', 'Sister')
+    )
+
+    # the common name for the house, e.g. 1329 Amberwick, 2102 Grace
+    name = models.CharField(max_length=50)
+
+    # the actual mailing address of the house
+    address = models.CharField(max_length=200)
+
+    # whether this is a brother's house or a sister's house
+    gender = models.CharField(max_length=1, choices=GENDER)
+
+    # whether this house is actively used by the training
+    used = models.BooleanField()
+
+
+class Bunk(models.Model):
+
+    POSITION = (
+        ('T', 'Top'),
+        ('B', 'Bottom')
+    )
+
+    # the bunk's number
+    number = models.SmallIntegerField()
+
+    # whether this is a top or bottom bunk
+    position = models.CharField(max_length=1, choices=POSITION)
+
+    # which house this bunk is in
+    house = models.ForeignKey(House)
+
+    # which trainee is in this bunk
+    trainee = models.OneToOneField('Trainee')
