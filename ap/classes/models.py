986598609be1c727040da8c2dd3726b787f94fcd
Jonathan Tien <jonathan.tien@gmail.com>
2013-06-10 20:38:39 -0700

    initial classes model

--- ap/classes/models.py
+++ ap/classes/models.py
@@ -1,3 +1,35 @@
 from django.db import models
 
-# Create your models here.
+"""" CLASSES models.py
+
+This is a utility module that will be used by other apps, such as class notes or
+class syllabi. It reprents FTTA classes such as God's Economy or Greek II.
+
+Each instance of a Class object represents a class *for a given term*
+
+Data Models:
+    - Class: a class in the FTTA
+
+"""
+
+
+class Class(models.Model):
+
+    CLASS_TYPE = (
+        ('MAIN', 'Main'),
+        ('1YR', '1st Year'),
+        ('2YR', '2nd Year'),
+        ('AFTN', 'Afternoon'),
+    )
+
+    # the name of this class, e.g. Full Ministry of Christ, or Character
+    name = models.CharField(max_length=50)
+
+    # the shortcode, e.g. FMoC or Char
+    code = models.CharField(max_length=5)
+
+    # which term this class is in
+    term = models.ForeignKey(Term)
+
+    # which type of class this is, e.g. Main, 1st year
+    type = models.CharField(max_length=4, choices=CLASS_TYPE)
