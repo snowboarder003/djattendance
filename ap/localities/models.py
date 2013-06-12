cd142cb6279faadecbb7988e522d25011f7c2f0b
Jonathan Tien <jonathan.tien@gmail.com>
2013-06-11 19:29:13 -0700

    initial model. state choices still need to be filled in

--- ap/localities/models.py
+++ ap/localities/models.py
@@ -1,3 +1,39 @@
 from django.db import models
 
-# Create your models here.
+""" LOCALITIES models.py
+
+The Localities module is a utility module underlying other apps. In particular,
+both trainees are related to localities (as being sent from), and teams are
+related to localities (as serving in).
+
+Data Models:
+    - Locality: a local church
+
+"""
+
+
+class Locality(models.Model):
+
+    city = models.CharField(max_length=50)
+
+    # optional for non US localities
+    state = models.CharField(max_length=2, blank=True, choices=STATES)
+
+    # for non US localities
+    province = models.CharField(max_length=30, blank=True)
+
+    country = models.CharField(max_length=5, choices=COUNTRIES)
+
+    STATES = (
+        ('AL','Alabama'),
+        ('',''),
+    )
+
+    COUNTRIES = (
+        ('USA', 'United States'),
+        ('NZ', 'New Zealand'),
+        ('AUS', 'Australia'),
+        ('CAN', 'Canada'),
+        ('CHN', 'China'),
+    )
+
