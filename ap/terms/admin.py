ee250cb6423f6c2d69bb288b39192d7d05139632
Sarah Rumbley <srumbley@mit.edu>
2013-06-22 16:22:44 -0700

    custom admin form for term

--- ap/terms/admin.py
+++ ap/terms/admin.py
@@ -1,18 +1,7 @@
 from django.contrib import admin
 from terms.models import Term
 
-##class TermAdmin(admin.ModelAdmin):
-##    fieldsets = [
-##        (None, {'fields': ['name']}),
-##    ]
-    
-        
-##    fieldsets = [
-##        (None, {'fields': ['name']}),
-##        ('Code', {'fields': ['code']}),
-##        ('Start', {'fields': ['start']}),
-##        ('End', {'fields': ['end']}),
-##    ]
-##    date_hierarchy = 'start'
+class TermAdmin(admin.ModelAdmin):
+    date_hierarchy = 'start'
 
 admin.site.register(Term)
