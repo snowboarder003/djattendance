1065f657ce1e3891fd6c3b72f6879773e8dc78ab
Enlai Xu <xu.olives@gmail.com>
2013-06-25 10:29:08 -0700

    ss

--- ap/ss/models.py
+++ ap/ss/models.py
@@ -1,100 +1,43 @@
 from django.db import models
+from service import *
 #from users.models import UserAccount
 
 #This is for service scheduler .
-#Define service and assign service to trainees
-
-
-#define service category such as Cleaning, Guard etc
-class serviceCategory(models.Model):
-
-    name = models.CharField(max_length=200)
-    description = models.TextField()
-
-    #return services of this category
-    def getService(self):
-        return service.objects.filter(svCategory=self)
-
-
-#define service such as Breakfast Cleaning, Dinner Prep, Guard A, etc
-class service(models.Model):
-
-    svCategory = models.ForeignKey(serviceCategory)
-    name = models.CharField(max_length=1000)
-    isActive = models.BooleanField()
-    workLoad = models.IntegerField()
-
-
-#define service Period such as Pre-Training, FTTA regular week, etc
-class servicePeriod(models.Model):
-
-    name = models.CharField(max_length=200)
-
-    #which service is on this Period
-    service = models.ManyToManyField(service)
-
-    startDate = models.DateField('start date')
-    endDate = models.DateField('end date')
-
-    #return the services of this Period
-    def getService(self):
-        return service.objects.filter(svPeriod=self)
-
-
-#define one specific service instance such as Monday Break Prep, Monday Guard C, etc
-class serviceInstance(models.Model):
-
-    WEEKDAY = (
-        ('Sun', 'Sunday'),
-        ('Mon', 'Monday'),
-        ('Tue', 'Tuesday'),
-        ('Wed', 'Wednesday'),
-        ('Thu', 'Thursday'),
-        ('Fri', 'Friday'),
-        ('Sat', 'Saturday'),
-    )
-
-    service = models.ForeignKey(service)
-    svPeriod = models.ForeignKey(servicePeriod)
-    weekday = models.CharField(max_length=3, choices=WEEKDAY)
-    startTime = models.TimeField('start time')
-    endTime = models.TimeField('end time')
-    recoveryTime = models.IntegerField('time')
-
+#Assign service to trainees
 
 #define service group such as Monday Prep Brothers, etc
-class serviceWorkerGroup(models.Model):
+class workerGroup(models.Model):
 
     name = models.CharField(max_length=200)
     service = models.ForeignKey(service)
-    svPeriod = models.ForeignKey(servicePeriod)
+    period = models.ForeignKey(period)
     numberOfWorkers = models.IntegerField()
     isActive = models.BooleanField()
 
 
 #service scheduler
-class serviceScheduler(models.Model):
+class scheduler(models.Model):
 
-    svPeriod = models.ForeignKey(servicePeriod)
+    period = models.ForeignKey(period)
     startDate = models.DateField()
     modifiedTime = models.DateTimeField()
     #TODO Trainee = models.ForeignKey(Trainee)
 
 
 #service assignment
-class serviceAssignment(models.Model):
+class assignment(models.Model):
 
     #TODO Trainee=models.ForeignKey(Trainee)
-    svScheduler = models.ForeignKey(serviceScheduler)
-    svWorkerGroup = models.ForeignKey(serviceWorkerGroup)
+    scheduler = models.ForeignKey(scheduler)
+    workerGroup = models.ForeignKey(workerGroup)
     isAbsent = models.BooleanField()
     #subTrainee = models.ForeignKey(Trainee)
 
 
 #service missed, not necessary
-class serviceMissed(models.Model):
+class missed(models.Model):
 
     #TODO Trainee = models.ForeignKey(Trainee)
-    svScheduler = models.ForeignKey(serviceScheduler)
-    svWorkerGroup = models.ForeignKey(serviceWorkerGroup)
+    scheduler = models.ForeignKey(scheduler)
+    workerGroup = models.ForeignKey(workerGroup)
 
