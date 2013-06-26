bb0c88a8b55f41f33871807c32c1a355bd98551f
Zuriel <mjfolwarski@gmail.com>
2013-06-26 15:57:20 -0700

    models refactor

--- ap/Accounts/models.py
+++ ap/Accounts/models.py
@@ -1,41 +1,50 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
-from users.models import UserAccount
+from datetime import date
 
 # Create your models here.
+def __get_age(self):
+        "returns the age"
+        today = date.today()
+        birth = date.fromtimestamp(self.birthdate)
+        try: 
+            birthday = birth.replace(year=today.year)
+        except ValueError: # raised when birth date is February 29 and the current year is not a leap year
+            birthday = birth.replace(year=today.year, day=birth.day-1)
+        if birthday > today:
+            return today.year - birth - 1
+        else:
+            return today.year - birth
 
+class UserAccount(AbstractUser):
+    middleName = models.CharField(max_length=30)
+    nickname = models.CharField(max_length=30)
+    maidenName = models.CharField(max_length=30)
+    birthdate = models.DateField() 
+    gender = models.CharField(max_length = 10) 
+    address = models.ForeignKey(util.address)
+    age = property(__get_age)
+    maritalStatus = models.BooleanField()
+        
 class TraineeAccount(AbstractUser):
-    user = models.OneToOneField(UserAccount.user)
-    term = models.OneToOneField(term.pk)
+    user = models.ForeignKey(UserAccount)
+    term = models.ManyToManyField(term.term)
     type = models.CharField(max_length = 30)
-    spouse = models.OneToOneField(TraineeAccount.pk)
-    emergencyInfo = models.OneToOneField(emergencyInfo.pk)
+    spouse = models.ForeignKey("TraineeAccount")
+    emergencyInfo = models.ForeignKey(emergencyInfo)
     dateBegin = models.DateField()
     dateEnd = models.DateField()
     degree = models.CharField(max_length=30)
-    mentor = models.OneToOneField(TraineeAccount.pk)
-    vehicle = models.OneToOneField(vehicle.pk)
-    schedule = models.OneToOneField(schedule.pk)
-    team = models.OneToOneField(team.pk)
-    services = models.ManyToManyField(services.pk)
+    mentor = models.ForeignKey("TraineeAccount")
+    vehicle = models.ForeignKey(util.vehicle)
+    schedule = models.ForeignKey(ss.schedule)
+    team = models.ForeignKey(team.team)
+    services = models.ManyToManyField(services.services)
     gospelPreferences = models.CharField()
-    house = models.OneToOneField(house.pk)
-    TA = models.OneToOneField(TA.pk)
-   
-class TrainintgAssistantAccount(AbstractUser):
+    house = models.ForeignKey(houses.house)
+    TA = models.ForeignKey("TrainingAssistantAccount")
+    bunkNumber = models.ForeignKey(house.bunk)
+    
+class TrainintgAssistantAccount(UserAccount):
     user = models.OneToOneField(UserAccount.user)
     maritalStatus = models.BooleanField
-    residence = models.CharField(max_length=30)
-   
-
-class UserAccount(AbstractUser):
-    user = models.OneToOneField(User)
-    birthdate = models.DateField() 
-    gender = models.CharField(max_length = 10) 
-    address = models.CharField(max_length = 100)
-    service = models.CharField(max_length = 30)
-    active = models.BooleanField()
-    phone_number = models.IntegerField()
-    locality = models.CharField(max_length = 30)
-    marital_status = models.CharField(max_length = 10)
-    
