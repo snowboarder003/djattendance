94a40a4c63b97c283cd0e24b28cff73b98047528
Anatol Tsang <fermat200pg@gmail.com>
2013-06-27 14:32:23 -0700

    Fixed things in models.py for Utils: City was being referenced as a foreign key before it was declared, added comments for make and model of Vehicle data model, changed field names to not be camel case, and fixed a small minus sign versus equal sign bug, and renamed stateOrProvince to region.

--- ap/utils/models.py
+++ ap/utils/models.py
@@ -6,31 +6,12 @@ The UTILS model handles various miscellaneous data models that will be used
 widely across other files.
 
 Data Models:
+	- City: A standard city anywhere in the world, used in localities and
+    addresses
     - Address: A standard US address used for training residences, emergency
     contact information, and other things
-    - City: A standard city anywhere in the world, used in localities and
-    addresses
     - Vehicle: Represents vehicles owned by trainees
 """
-
-
-class Address(models.Model):
-	# line 1 of the address field
-	address1 = models.CharField(max_length=150)
-	
-	# line 2 of the address field
-	address2 = models.CharField(max_length=150)
-	
-	# City foreign key
-	city = models.ForeignKey(City)
-	
-	zipCode = models.CharField(max_length=5)
-
-	# optional four-digit zip code extension
-	zip4 = models.CharField(max_length=4)
-
-	# optional details field
-	details = models.CharField(max_length=150)
 
 
 class City(models.Model):
@@ -39,18 +20,39 @@ class City(models.Model):
 	name = models.CharField(max_length=50)
 	
 	# state or province (depending on which country it is)
-	stateOrProvince = models.CharField(max_length=30)
+	region = models.CharField(max_length=30)
 
 	country = models.CharField(max_length=50)
 
 
+class Address(models.Model):
+	# line 1 of the address field
+	address1 = models.CharField(max_length=150)
+	
+	# line 2 of the address field
+	address2 = models.CharField(max_length=150, blank=True)
+	
+	# City foreign key
+	city = models.ForeignKey(City)
+	
+	zip_code = models.SmallIntegerPositiveField()
+
+	# optional four-digit zip code extension
+	zip_4 = models.SmallPositiveIntegerField(blank=True)
+
+	# optional details field
+	details = models.CharField(max_length=150)
+
+
 class Vehicle(models.Model):
 	
+	# e.g. Honda, Toyota
 	make = models.CharField(max_length=30)
 	
-	model - models.CharField(max_length=30)
+	# e.g. Accord, Camry
+	model = models.CharField(max_length=30)
 	
-	licensePlate = models.CharField(max_length=10)
+	license_plate = models.CharField(max_length=10)
 
 	# Trainee foreign key (will uncomment when imported)
 	# trainee = models.ForeignKey(Trainee.Trainee)
