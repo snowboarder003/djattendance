994053447214f163992fa1e1c7dbefbac529638c
Tiffany <tiffanyhua@gmail.com>
2013-06-27 15:15:00 -0700

    changed password and user to database to standard

--- ap/ap/settings/base.py
+++ ap/ap/settings/base.py
@@ -15,8 +15,8 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
         'NAME': 'ap',                      # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
-        'USER': 'postgres',
-        'PASSWORD': '7goldfishes',
+        'USER': 'django',
+        'PASSWORD': 'attend2god',
         'HOST': '',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
         'PORT': '5432',                      # Set to empty string for default.
     }
