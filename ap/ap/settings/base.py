6000198c1d2b4dffadb7444d6f84bf62ec1486d9
Sarah Rumbley <srumbley@mit.edu>
2013-06-27 15:37:07 -0700

    fixed bug in SITE_ROOT

--- ap/ap/settings/base.py
+++ ap/ap/settings/base.py
@@ -5,7 +5,8 @@ import django
 # calculated paths for django and the site
 # used as starting points for various other paths
 DJANGO_ROOT = os.path.dirname(os.path.realpath(django.__file__))
-SITE_ROOT = os.path.dirname(os.path.realpath(__file__))
+SITE_ROOT = os.path.dirname(os.path.dirname(os.path.dirname(os.path.realpath(__file__))))
+print(SITE_ROOT)
 
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
