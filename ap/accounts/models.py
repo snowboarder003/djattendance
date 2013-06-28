86be1028baa530f42d1a802daf0a803078db5b99
Zuriel <mjfolwarski@gmail.com>
2013-06-27 21:27:10 -0700

    corrections to get_age

--- ap/accounts/models.py
+++ ap/accounts/models.py
@@ -1,10 +1,20 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 from datetime import date
-import util
+from utilities.models import Vehicle, Address
+from term_stub.models import Term
+from teams_stub.models import Teams
 
 # Create your models here.
-def __get_age(self):
+
+class UserAccount(AbstractUser):
+    middleName = models.CharField(max_length=30)
+    nickname = models.CharField(max_length=30)
+    maidenName = models.CharField(max_length=30)
+    birthdate = models.DateField() 
+    gender = models.CharField(max_length = 10) 
+    address = models.ForeignKey(Address)
+    def get_age(self):
         "returns the age"
         today = date.today()
         birth = date.fromtimestamp(self.birthdate)
@@ -17,27 +27,20 @@ def __get_age(self):
         else:
             return today.year - birth
 
-class UserAccount(AbstractUser):
-    middleName = models.CharField(max_length=30)
-    nickname = models.CharField(max_length=30)
-    maidenName = models.CharField(max_length=30)
-    birthdate = models.DateField() 
-    gender = models.CharField(max_length = 10) 
-    address = models.ForeignKey(util.address)
-    age = property(__get_age)
+    age = property(get_age)
     maritalStatus = models.BooleanField()
         
 class TraineeAccount(AbstractUser):
     user = models.ForeignKey(UserAccount)
-    term = models.ManyToManyField(term.term)
+    term = models.ManyToManyField(Term)
     type = models.CharField(max_length = 30)
     spouse = models.ForeignKey("TraineeAccount")
-    emergencyInfo = models.ForeignKey(emergencyInfo)
+    emergencyInfo = models.ForeignKey("emergencyInfo")
     dateBegin = models.DateField()
     dateEnd = models.DateField()
     degree = models.CharField(max_length=30)
     mentor = models.ForeignKey("TraineeAccount")
-    vehicle = models.ForeignKey(util.models.Vehicle)
+    vehicle = models.ForeignKey(utils.vehicle)
     schedule = models.ForeignKey(ss.schedule)
     team = models.ForeignKey(team.team)
     services = models.ManyToManyField(services.services)
