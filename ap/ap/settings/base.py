a5c217ea4ee449567126fd6740670ec3447a803d
Tiffany <tiffanyhua@gmail.com>
2013-06-27 21:07:31 -0700

    Changed database settings to postgres in base.py

--- ap/ap/settings/base.py
+++ ap/ap/settings/base.py
@@ -12,8 +12,8 @@ MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '',                      # Or path to database file if using sqlite3.
+        'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'ap',                      # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
         'USER': 'django',
         'PASSWORD': 'attend2god',
@@ -121,6 +121,7 @@ INSTALLED_APPS = (
     'django.contrib.sites',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'books',
     # Uncomment the next line to enable the admin:
     # 'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
