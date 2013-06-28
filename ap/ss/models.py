fc11577f1cba76ee1f25cc91e2d3aa9c6bba79dd
Enlai Xu <xu.olives@gmail.com>
2013-06-27 21:32:06 -0700

    modified scheduler model

--- ap/ss/models.py
+++ ap/ss/models.py
@@ -36,21 +36,12 @@ class Instance(models.Model):
     #after doing a service especially guard service , a trainee should have rest time(recoveryTime) for next time.
     recoveryTime = models.IntegerField('time')
 
-    #get instances by service period and service
-    def getInstancesByService(self, period, service):
-        return Instance.objects.filter(service=service, period=period)
-
-    #get instances of service of current week ordered by time
-    def getInstancesOrderByTime(self):
-        #get the current service period according to current datetime
-        _current_date = datetime.now().date()
-        period = Period.objects.filter(endTime_gte=_current_date, startTime_lte=_current_date)
-        return Instance.objects.filter(period=period).order_by("startTime")
-
 
 #define Service group such as Monday Prep Brothers, etc
 #From example, Monday Breakfast Prep includes Kitchen Star, Brother, Sister
 class WorkerGroup(models.Model):
+
+    """define worker groups of each service instance"""
 
     name = models.CharField(max_length=200)
     instance = models.ForeignKey(Instance)
@@ -60,17 +51,6 @@ class WorkerGroup(models.Model):
     #Some service instance is not designated, but some service worker group might be designated such Kitchen Star.
     isDesignated = models.BooleanField()
 
-    #return the the WorkerGroup of a certain Instance
-    def getWorkerGroup(self, instance):
-        return WorkerGroup.objects.filter(instance=instance)
-
-    def getNonDesignateGroupOrderByTime(self):
-        """return the None-Designation Worker Group Order By Time of current week"""
-        _current_date = datetime.now().date()
-        period = Period.objects.filter(endTime_gte=_current_date, startTime_lte=_current_date)
-        return WorkerGroup.objects.filter(~Q(instance_service_category="Designated"), instance_period=period).\
-            order_by("instance_startTime")
-
 
 #Service qualification group such as sack lunch star,Kitchen Star, House Inspector, Shuttle Driver, Piano, Usher, etc
 #This is a not permanent designation. For example, two brothers might be designated as AV brothers, but others brothers
@@ -79,7 +59,6 @@ class QualificationGroup(models.Model):
 
     name = models.CharField(max_length=200)
     Trainee = models.ManyToManyField(TraineeAccount)
-
     workerGroup = models.ForeignKey(WorkerGroup)
 
 
@@ -118,14 +97,16 @@ class Scheduler(models.Model):
         workerGroup = WorkerGroup()
 
         #get the non designated worker groups of current week
+        #TO IMPROVE: order the workerGroup later by the ratio of
+        #           the number of available trainees and the number of the needed trainees.
         workerGroups = workerGroup.getNonDesignateGroupOrderByTime()
 
         #get user service assignment history, use a list of dicts to store the history
         #trainees[{TraineeId:13, workLoad:10, previousService: 1}, {}, {}]
         #this list is easy to sort the list of dict.
 
-        #TO IMPROVE: If want to reduce the time, use another list of trainee_track to track whether the history of a certain trainee
-        #since list is easy to search the index by value
+        #TO IMPROVE: If want to reduce the time, use another list of trainee_track to track whether the history of
+        #  a certain trainee since list is easy to search the index by value
         #is already in the list of history dict.if not then add new history to the history list dict another wise skip.
 
         #Or to use OrderDict: for example dict_previousSv{traineeID:svId,...}, dict_thisweekWork{traineeId, workLoad..}
@@ -136,32 +117,128 @@ class Scheduler(models.Model):
         for i in range(0, trainees.count()):
             historyList[i] = self.getWorkLoadHistory(trainees[i])
 
-        #Enumerate the worker groups to assign the services to different trainees
+        #Enumerate the worker groups to count the available number of trainees of each workerGroups
         for group in workerGroups:
-            #ToDO get the available trainees
-            #ToDO get the trainees service history
-            #ToDo order the trainees according to their previous history
-            #ToDo order the trainees according to their current week history(total workload and recovery time)
-            #ToDo consider other
-            pass
+            trainees = self.getAvailableTrainees(group)
+            #TODO sort workerGroups according the the number of available trainees
+
+        listAssignment = dict(workerGroups.count())
+
+        #Enumerate the worker groups to assign the services
+        for i in range(workerGroups.count()):
+            group = workerGroups[i]
+            trainee = self.getBestCandidate(trainees, group)
+            #ToDo assign the trainee
+
+            assignment = Assignment()
+            assignment.scheduler = self
+            assignment.workerGroup = group
+            assignment.trainee = trainee
+            listAssignment[trainee] = assignment
 
     def getWorkLoadHistory(self, trainee):
         """Get the trainee's service assignment history"""
         pass
 
+    def getAvailableTrainees(self, workerGroup):
+        """get the available trainee list of workerGroup"""
+        pass
+
+    def checkConflict(self, trainee, workerGroups):
+        """check the whether the trainee has the schedule conflict with his current service"""
+        pass
+
+    def getBestCandidate(self, trainees, group):
+        pass
+
     def getMissedInstances(self):
         """return missed services of current scheduler"""
-        return self.assignment_set.all()
+        return self.assignment_set.filter(isAbsent=1)
+
+        #get instances by service period and service
+    def getInstancesByService(self, period, service):
+        return Instance.objects.filter(service=service, period=period)
+
+    #get instances of service of current week ordered by time
+    def getInstancesOrderByTime(self):
+        #get the current service period according to current datetime
+        _current_date = datetime.now().date()
+        period = Period.objects.get(endDate__gte=_current_date, startDate__lte=_current_date)
+        return Instance.objects.filter(period=period).order_by("startTime")
+        #return Instance.objects.filter(period=period).order_by("service")
+
+    #return the the WorkerGroup of a certain Instance
+    def getWorkerGroup(self, instance):
+        return WorkerGroup.objects.filter(instance=instance)
+
+    def getNonDesignateGroupOrderByTime(self):
+        """return the None-Designation Worker Group Order By Time of current week"""
+        _current_date = datetime.now().date()
+        period = Period.objects.get(endDate__gte=_current_date, startDate__lte=_current_date)
+        return WorkerGroup.objects.filter(~Q(instance_service_category="Designated"), instance_period=period). \
+            order_by("instance_startTime")
+
+    #---------------------------------------------------------------------------------------------------#
+    #following functions are for testing and debugging
+    def test(self):
+        pass
+
+    def importWorkerGroup(self):
+        #delete unused workergroup
+        """wgs = workerGroup_test.objects.all().order_by('period', 'service')
+        for wg in wgs:
+            ins = Instance.objects.filter(period=wg.period, service=wg.service)
+            if ins.count() > 0:
+                wg.instance = ins[0]
+               # wg.save()
+            else:
+                wg.delete()
+                print wg.service.name + "   " + wg.period.name
+
+        #import workerGroup
+        wgs = workerGroup_test.objects.all()
+        for wg in wgs:
+            wg_tmp = WorkerGroup()
+            wg_tmp.id = wg.id
+            wg_tmp.numberOfWorkers = wg.numberOfWorkers
+            wg_tmp.name = wg.name
+            wg_tmp.instance = wg.instance
+            wg_tmp.isDesignated = wg.isDesignated
+            wg_tmp.isActive = wg.isActive
+            wg_tmp.save()"""
+
+    def printWorkerGroups(self):
+        pass
+
+    def printService(self):
+        cgs = Category.objects.all()
+        for cg in cgs:
+            print cg.name
+            svs = cg.getServices()
+            for sv in svs:
+                print "   " + sv.name
+                pds = Period.objects.filter(service=sv)
+                for pd in pds:
+                    print "     " + pd.name
+                    ins = Instance.objects.filter(period=pd, service=sv)
+                    if ins.count() > 0:
+                        print "         " + "StarTime:" + ins[0].startTime.strftime("%H-%M-%S") +\
+                              "EndTime:" + ins[0].endTime.strftime("%H-%M-%S")
+                    else:
+                        print "         None"
 
 
 #Service Assignment
 class Assignment(models.Model):
 
-    #TODO Trainee=models.ForeignKey(Trainee)
+    trainee = models.ForeignKey(TraineeAccount, related_name="assignment_trainee")
     scheduler = models.ForeignKey(Scheduler)
     workerGroup = models.ForeignKey(WorkerGroup)
     isAbsent = models.BooleanField()
-    subTrainee = models.ForeignKey(TraineeAccount)
+    subTrainee = models.ForeignKey(TraineeAccount, related_name="assignment_subTrainee")
+
+    def getAssignmentsByTrainee(self, trainee):
+        return WorkerGroup.objects.filter(trainee=trainee)
 
 
 #permanent designation
@@ -171,5 +248,8 @@ class PermanentDesignation(models.Model):
     workerGroup = models.ManyToManyField(WorkerGroup)
     isActive = models.BooleanField()
 
-    def getAssignmentByTrainee(self, trainee):
-        return WorkerGroup.objects.filter(trainee=trainee)
+
+
+
+
+
