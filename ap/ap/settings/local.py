e0067e50c8c2597fa1e9729da2c8ad61a6446c39
Jonathan Tien <jonathan.tien@gmail.com>
2013-06-28 23:38:58 -0700

    changed local db settings (added localhost)

--- ap/ap/settings/local.py
+++ ap/ap/settings/local.py
@@ -10,7 +10,7 @@ DATABASES = {
         # The following settings are not used with sqlite3:
         'USER': 'ap',
         'PASSWORD': '',
-        'HOST': '',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
+        'HOST': 'localhost',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
         'PORT': '',                      # Set to empty string for default.
     }
 }
