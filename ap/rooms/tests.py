8793564346a3d39b29eb511975a167a793b17305
Anatol Tsang <fermat200pg@gmail.com>
2013-06-29 15:40:47 -0700

    Changed formatting of rooms/tests.py to conform to PEP 8.

--- ap/rooms/tests.py
+++ ap/rooms/tests.py
@@ -8,11 +8,18 @@ from django.test import TestCase
 from rooms.models import Room
 
 class RoomsTest(TestCase):
-	def setUp(self):
-		r=Room.objects.create(code="154B", name="West Side Third Heavens", floor=3, type="SR", access="S")
-	def test_room_returns_right_value(self):
-		"""
-		"""
-		r=Room.objects.get(code="154B")
-		self.assertEqual(r.type, "SR")
-		self.assertEqual(r.access, "S")
+    def setUp(self):
+        r = Room.objects.create(
+            code="154B", 
+            name="West Side Third Heavens", 
+            floor=3, 
+            type="SR", 
+            access="S"
+        )
+
+    def test_room_returns_right_value(self):
+        """
+        """
+        r = Room.objects.get(code="154B")
+        self.assertEqual(r.type, "SR")
+        self.assertEqual(r.access, "S")
