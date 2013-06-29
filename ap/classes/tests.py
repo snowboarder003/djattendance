fcfe6fcd8fa80e5220cb43e58c50bd184ea750f4
Anatol Tsang <fermat200pg@gmail.com>
2013-06-29 15:32:12 -0700

    Removed unneccessary whitespace in classes/test.py.

--- ap/classes/tests.py
+++ ap/classes/tests.py
@@ -15,7 +15,7 @@ class ClassTests(TestCase):
         c2=Class.objects.create(name='God Ordained Way', code='GOW',type='MAIN')
         c3=Class.objects.create(name='New Jerusalem', code='NJ',type='2YR')
         c4=Class.objects.create(name='Character', code='CHAR',type='AFTN')
-        
+
     def test_class_type_choices(self):
         """
         Tests that the choice types matches
@@ -30,7 +30,7 @@ class ClassTests(TestCase):
         c2=Class.objects.get(code='GOW')
         c3=Class.objects.get(code='NJ')
         c4=Class.objects.get(code='CHAR')
-        
+
         self.assertEqual(c1.type, "1YR")
         self.assertEqual(c2.type, "MAIN")
         self.assertEqual(c3.type, "2YR")
