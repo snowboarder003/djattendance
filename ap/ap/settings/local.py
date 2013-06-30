45f20e56b204faf6855ff850c3a69a7d620e36b6
Jonathan Tien <jonathan.tien@gmail.com>
2013-06-30 11:32:35 -0700

    rolled back changes to local.py, restored db settings, esp localhost

--- ap/ap/settings/local.py
+++ ap/ap/settings/local.py
@@ -9,9 +9,9 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',  # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
         'NAME': 'ap',                      # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
-        'USER': 'django',
-        'PASSWORD': 'attend2god',
-        'HOST': '',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
+        'USER': 'ap',
+        'PASSWORD': '',
+        'HOST': 'localhost',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
         'PORT': '',                      # Set to empty string for default.
     }
 }
