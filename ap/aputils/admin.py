2d429e6429fc81196c34bc4f073b5a50d004c37a
Anatol Tsang <fermat200pg@gmail.com>
2013-06-29 20:53:14 -0700

    Beefed up the admin.py file for aputils.

--- ap/aputils/admin.py
+++ ap/aputils/admin.py
@@ -2,7 +2,42 @@ from django.contrib import admin
 
 from aputils.models import Country, City, Address, Vehicle
 
-admin.site.register(Country)
-admin.site.register(City)
-admin.site.register(Address)
-admin.site.register(Vehicle)
+class AddressAdmin(admin.ModelAdmin):
+    list_display = (
+        'address1', 
+        'address2', 
+        'city', 
+        'zip_code', 
+        'zip4', 
+        'details'
+    )
+
+
+class CityAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'region',
+        'country'
+    )
+
+
+class CountryAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'code'
+    )
+
+
+class VehicleAdmin(admin.ModelAdmin):
+    list_display = (
+        'license_plate',
+        'color',
+        'make',
+        'model'
+    )
+
+
+admin.site.register(Address, AddressAdmin)
+admin.site.register(City, CityAdmin)
+admin.site.register(Country, CountryAdmin)
+admin.site.register(Vehicle, VehicleAdmin)
