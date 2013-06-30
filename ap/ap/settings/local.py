e6d12a1f4d1abdcf64c83812baf02c5ea25e436d
Sarah Rumbley <srumbley@mit.edu>
2013-06-29 17:21:20 -0700

    don't know why i'm committing local.py

--- ap/ap/settings/local.py
+++ ap/ap/settings/local.py
@@ -5,14 +5,12 @@ TEMPLATE_DEBUG = True
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',  # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
         'NAME': 'ap',                      # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
-        'USER': 'ap',
+        'USER': 'django',
         'PASSWORD': '',
         'HOST': 'localhost',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
         'PORT': '',                      # Set to empty string for default.
     }
 }
-
-
