a1f61ee5901a7bd11f5037ded7abffeacd14b265
Jonathan Tien <jonathan.tien@gmail.com>
2013-07-28 14:01:44 -0700

    added accounts back to lilst of installed apps

--- ap/ap/settings/base.py
+++ ap/ap/settings/base.py
@@ -118,7 +118,7 @@ INSTALLED_APPS = (
     'django.contrib.staticfiles',
     'django.contrib.admin',
     'django.contrib.admindocs',
- #   'accounts',
+    'accounts',
     'aputils',
     'books',
     'classes',
