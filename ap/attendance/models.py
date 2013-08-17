a58feddfa9cb7bc1930cbf7510657688e9cd3d4d
Jonathan Tien <jonathan.tien@gmail.com>
2013-08-17 11:35:22 -0700

    edited comments, added blank=True for monitor field

--- ap/attendance/models.py
+++ ap/attendance/models.py
@@ -3,12 +3,13 @@ from schedules.models import Event
 from accounts.models import Trainee
 from terms.models import Term
 
-""" attendance models.py 
-The attendance module takes care of data and logic related to tracking attendance.
-It does not handle things such as schedules or leave slips. 
+""" attendance models.py
+The attendance module takes care of data and logic directly related
+to tracking attendance. It does not handle things such as schedules
+or leave slips.
 
 DATA MODELS:
-    - Roll: an attendance record per trainee, per event. 
+    - Roll: an attendance record per trainee, per event.
             for example, if 10 trainees are supposed to be at an event,
             then there will be 10 roll objects associated to that event,
             as well as each trainee.
@@ -37,7 +38,8 @@ class Roll(models.Model):
 
     notes = models.CharField(max_length=200)
 
-    monitor = models.ForeignKey(Trainee)
+    # the one who submitted this roll
+    monitor = models.ForeignKey(Trainee, blank=True)
 
     timestamp = models.DateTimeField()
 
