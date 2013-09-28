0494d04c67660c036eedbdaa5af594e024541955
Sarah Rumbley <srumbley@mit.edu>
2013-09-28 16:14:47 -0700

    removed unnecessary lines from views.py

--- ap/ap/views.py
+++ ap/ap/views.py
@@ -1,8 +1,5 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
-from django.views.generic.edit import FormView
-# from ap.forms import AbsentTraineeForm
-
 
 @login_required
 def home(request):
