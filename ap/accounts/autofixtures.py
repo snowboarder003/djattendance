86508f7a77162322f1cd77c4acccaf33c27c3e2d
Jonathan Tien <jonathan.tien@gmail.com>
2013-10-04 08:54:53 -0700

    more details

--- ap/accounts/autofixtures.py
+++ ap/accounts/autofixtures.py
@@ -1,22 +1,37 @@
+import random
+
 from accounts.models import User, TrainingAssistant, Trainee
-from autofixture import generators, register, AutoFixture
+from autofixture import generators, register, AutoFixture, Generator
+
+
+class LastNameGenerator(Generator):
+	""" Generates a last name """
+
+	def __init__(self):
+		self.surname = []
+
+	def generate(self):
+		return self.surname[random.randint(0, len(self.surname))]
 
 
 class UserAutoFixture(AutoFixture):
     field_values = {
+    	'email' : generators.EmailGenerator(),
+    	'firstname' : ,
+    	'lastname' :
     }
 
 register(User, UserAutoFixture)
 
 
-class TraineeAutoFixture(AutoFixture):
+class TraineeAutoFixture(AutoFixture, generate_fk=['account']):
 	field_values = {
     }
 
 register(Trainee, TraineeAutoFixture)
 
 
-class TrainingAssistantAutoFixture(AutoFixture):
+class TrainingAssistantAutoFixture(AutoFixture, generate_fk=['account']):
 	field_values = {
     }
 
