054e5b982e94057e1c5631a0822e6e25fe3111e9
Sarah Rumbley <srumbley@mit.edu>
2013-10-12 15:23:08 -0700

    changed folder name from camelCase to underscores

--- ap/absentTraineeRoster/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from absentTraineeRoster.models import Roster, Entry
-
-class AbsentTraineeForm(forms.ModelForm):
-	class Meta:
-		model = Entry
-		fields = ('absentee', 'reason', 'comments')
--- ap/absentTraineeRoster/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls import patterns, url
-
-from absentTraineeRoster import views
-
-urlpatterns = patterns('',
-	# url(r'^attendance_form/$', views.attendance_form, name='attendance_form'),
-	url(r'^attendance_form/$', views.AbsentTraineeRosterView.as_view(), name='attendance_form'),
-	
-)
