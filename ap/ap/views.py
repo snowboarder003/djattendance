d41734d757e60de0dc70a1b901cfbcfaa7608d57
Sarah Rumbley <srumbley@mit.edu>
2013-10-14 17:22:39 -0700

    added base_example.html

--- ap/ap/views.py
+++ ap/ap/views.py
@@ -6,5 +6,5 @@ from django.contrib.auth.decorators import login_required
 def home(request):
     return render(request, 'index.html')
 
-# def base_example(request):
-# 	return render(request, 'base_example.html')
+def base_example(request):
+	return render(request, 'base_example.html')
