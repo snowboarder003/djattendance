73ea8985a6ed85b2d9cc15a2724a3678caf45b30
Tiffany <tiffanyhua@gmail.com>
2013-10-19 16:05:23 -0700

    implemented dynamically add form to formset using jquery in absent_trainee_form

--- ap/absent_trainee_roster/views.py
+++ ap/absent_trainee_roster/views.py
@@ -3,6 +3,9 @@ from django.shortcuts import render, render_to_response
 from django.views.generic.edit import FormView
 from absent_trainee_roster.forms import AbsentTraineeForm
 from absent_trainee_roster.models import Entry
+from django.core.context_processors import csrf
+from django.template import RequestContext # For CSRF
+from django.forms.formsets import formset_factory, BaseFormSet
 	
 # @login_required, permissions limited to HC's
 class AbsentTraineeFormView(FormView):
@@ -14,7 +17,13 @@ class AbsentTraineeFormView(FormView):
 		return super(AbsentTraineeFormView, self).form_valid(form)
 
 def absent_trainee_form(request):
-	EntryFormSet = modelformset_factory(Entry, AbsentTraineeForm, max_num=10, extra=2)
+	#This class is used to make empty formset forms required
+	class RequiredFormSet(BaseFormSet):
+		def __init__(self, *args, **kwargs):
+			super(RequiredFormSet, self).__init__(*args, **kwargs)
+			for form in self.forms:
+				form.empty_permitted = False
+	EntryFormSet = formset_factory(AbsentTraineeForm, max_num=10, formset=RequiredFormSet)
 	if request.method == 'POST':
 		formset = EntryFormSet(request.POST, request.FILES)
 		if formset.is_valid():
@@ -22,4 +31,5 @@ def absent_trainee_form(request):
 			# do something with formset.cleaned_data
 	else:
 		formset = EntryFormSet()
+	
 	return render_to_response('absent_trainee_roster/absent_trainee_form.html', {'formset': formset, 'user':request.user})
