003340fc86da0bc4b85455b1518aee87ef88527b
Jonathan Tien <jonathan.tien@gmail.com>
2013-10-19 15:36:14 -0700

    added a bunch of django_debug_toolbar stuff, but didn't get it working yet

--- ap/ap/settings/local.py
+++ ap/ap/settings/local.py
@@ -3,6 +3,9 @@ from .base import *
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
+INSTALLED_APPS += ('debug_toolbar',)
+
+INTERNAL_IPS = ('127.0.0.1',)
 
 DATABASES = {
     'default': {
@@ -15,3 +18,15 @@ DATABASES = {
         'PORT': '',                      # Set to empty string for default.
     }
 }
+
+DEBUG_TOOLBAR_PANELS = (
+    'debug_toolbar.panels.version.VersionDebugPanel',
+    'debug_toolbar.panels.timer.TimerDebugPanel',
+    'debug_toolbar.panels.settings_vars.SettingsVarsDebugPanel',
+    'debug_toolbar.panels.headers.HeaderDebugPanel',
+    'debug_toolbar.panels.request_vars.RequestVarsDebugPanel',
+    'debug_toolbar.panels.template.TemplateDebugPanel',
+    'debug_toolbar.panels.sql.SQLDebugPanel',
+    'debug_toolbar.panels.signals.SignalDebugPanel',
+    'debug_toolbar.panels.logger.LoggingPanel',
+)
