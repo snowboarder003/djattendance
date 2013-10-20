5dedcea866b31ae7be4648f653f0dd85bca6153a
Jonathan Tien <jonathan.tien@gmail.com>
2013-10-19 20:09:33 -0700

    added approve admin action

--- ap/dailybread/admin.py
+++ ap/dailybread/admin.py
@@ -2,7 +2,11 @@ from django.contrib import admin
 
 from .models import Portion
 
+def approve(modeladmin, request, queryset):
+    queryset.update(approved=True)
+
 class PortionAdmin(admin.ModelAdmin):
     list_display = ('approved', 'title', 'submitted_by', 'timestamp')
+    actions = [approve]
 
 admin.site.register(Portion, PortionAdmin)
