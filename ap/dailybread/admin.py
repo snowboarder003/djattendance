004e65108a98bb6b84b4815f80eba92f8e038006
Jonathan Tien <jonathan.tien@gmail.com>
2013-10-19 19:20:48 -0700

    added some options to make admin easier to use

--- ap/dailybread/admin.py
+++ ap/dailybread/admin.py
@@ -2,4 +2,7 @@ from django.contrib import admin
 
 from .models import Portion
 
-admin.site.register(Portion)
+class PortionAdmin(admin.ModelAdmin):
+    list_display = ('approved', 'title', 'submitted_by', 'timestamp')
+
+admin.site.register(Portion, PortionAdmin)
