92893951af52a2154c104481f9c09198467fdc10
Sarah Rumbley <srumbley@mit.edu>
2013-11-02 16:25:50 -0700

    formatted entry admin

--- ap/absent_trainee_roster/admin.py
+++ ap/absent_trainee_roster/admin.py
@@ -3,6 +3,15 @@ from django.contrib.auth.admin import UserAdmin
 
 from models import Absentee, Roster, Entry
 
+class EntryAdmin(admin.ModelAdmin):
+	def house(obj):
+		return obj.absentee.house
+
+	def term(obj):
+		return obj.absentee.term
+
+	list_display = ('roster', 'absentee', 'reason', 'comments', house,)
+
 admin.site.register(Absentee)
 admin.site.register(Roster)
-admin.site.register(Entry)
+admin.site.register(Entry, EntryAdmin)
