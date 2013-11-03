a371630fc291ce1f402ba6edae14aad616bdca6e
Jonathan Tien <jonathan.tien@gmail.com>
2013-11-02 20:00:46 -0700

    added settings/urls for managing/viewing uploaded badge pictures

--- ap/ap/settings/local.py
+++ ap/ap/settings/local.py
@@ -34,3 +34,12 @@ DEBUG_TOOLBAR_PANELS = (
 DEBUG_TOOLBAR_CONFIG = {
     'INTERCEPT_REDIRECTS': False,
 }
+
+# Absolute filesystem path to the directory that will hold user-uploaded files.
+# Example: "/var/www/example.com/media/"
+MEDIA_ROOT = '/Library/WebServer/Documents/djattendance/ap/media/'
+
+# URL that handles the media served from MEDIA_ROOT. Make sure to use a
+# trailing slash.
+# Examples: "http://example.com/media/", "http://media.example.com/"
+MEDIA_URL = '/media/'
--- ap/ap/urls.py
+++ ap/ap/urls.py
@@ -1,8 +1,9 @@
 from django.conf.urls import patterns, include, url
 from django.contrib.auth.views import login, logout
-
-# Uncomment the next two lines to enable the admin:
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
+
 import autofixture
 
 admin.autodiscover()
@@ -25,5 +26,5 @@ urlpatterns = patterns('',
 
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
-)
+) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
