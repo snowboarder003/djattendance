d7bd6ea8a4741788f1f9c318702057ba2d877137
Jonathan Tien <jonathan.tien@gmail.com>
2013-11-02 20:04:44 -0700

    added django-extensions

--- ap/ap/settings/local.py
+++ ap/ap/settings/local.py
@@ -3,7 +3,8 @@ from .base import *
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
-INSTALLED_APPS += ('debug_toolbar',)
+INSTALLED_APPS += ('debug_toolbar',
+                   'django_extensions',)
 
 INTERNAL_IPS = ('127.0.0.1',)
 
