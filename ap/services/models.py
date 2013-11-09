d2934bfb7eff67ef5c5a6bb8edfa59a8fb282096
Enlai Xu <xu.olives@gmail.com>
2013-11-09 14:54:19 -0800

    update to the lastest service, adding new feild needQualification in Service model

--- ap/services/models.py
+++ ap/services/models.py
@@ -17,59 +17,60 @@ This data model defines services in the training. We organize services in the fo
 
 """
 
+
+#Define Service Category such as Cleaning, Guard, etc.
 class Category(Group):
     """
-    Defines a service category such as Cleanup, Guard, Mopping, Chairs, etc.
+    Defines a service category such as Clean-up, Guard, Mopping, Chairs, etc.
     """
-    # name = models.CharField(max_length=75)
-    description = models.TextField(blank=True, null=True)
 
-    #return services of this Category
-    def getServices(self):
-        #return Service.objects.filter(category=self)
-        return self.service_set.all()
+    description = models.TextField()
 
     def __unicode__(self):
         return self.name
 
-    class Meta:
-        verbose_name_plural = "categories"
 
-
+#define Service such as Breakfast Cleaning, Dinner Prep, Guard A, etc
 class Service(Group):
-    """" FTTA service class to define service such as
-    Tues. Breakfast Cleanup, Dinner Prep, Guard A, Wednesday Chairs, etc.
-
+    """"
+        FTTA service class to define service such as
+    Breakfast Clean-up, Dinner Prep, Guard A, Wednesday Chairs, etc.
     This also includes designated services such as Accounting or Lights.
     """
 
-    description = models.TextField(blank=True, null=True)
-    category = models.ForeignKey(Category)
+    category = models.ForeignKey(Category, related_name="services")
     isActive = models.BooleanField()
 
-    # every service have different workload,
-    # for example guard is much more intense than cleaning
+    #Every service has different workload to describe its
+        #service hours and service intensity
     workload = models.IntegerField()
+
+        #whether this service needs certain qualified trainees
+    needQualification = models.BooleanField(blank=True)
+
+    #Service qualification such as Sack lunch star,Kitchen Star,
+    #Shuttle Driver, Piano, Usher, etc
+    #Note: This is different from permanent designation. For example,
+    #two brothers are be designated as AV brothers,
+    #but others brothers have the qualification to serve AV.
+    qualifiedTrainees = models.ManyToManyField('accounts.Trainee')
 
     def __unicode__(self):
         return self.name
 
 
+#Define Service Period such as Pre-Training, FTTA regular week, etc
 class Period(models.Model):
-    """define Service Period such as Pre-Training, FTTA regular week, etc"""
+    """Define Service Period such as Pre-Training, FTTA regular week, etc"""
 
     name = models.CharField(max_length=200)
     description = models.TextField()
 
-    #which Service is on this Period
-    service = models.ManyToManyField(Service)
+    #Service which is in this Period
+    service = models.ManyToManyField(Service, related_name="periods")
 
     startDate = models.DateField('start date')
     endDate = models.DateField('end date')
-
-    #return the services of this Period
-    def getServices(self):
-        return self.service.all()
 
     def __unicode__(self):
         return self.name
