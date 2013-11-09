a1923f0433f9890e5761185c116bcc1c1054eed2
Enlai Xu <xu.olives@gmail.com>
2013-11-09 15:43:17 -0800

    starting generating test data using autofixture

--- ap/ss/tests.py
+++ ap/ss/tests.py
@@ -7,9 +7,12 @@ Replace this with more appropriate tests for your application.
 
 from django.test import TestCase
 from ss.models import *
+from autofixture import *
 
 
 class SimpleTest(TestCase):
+
+    @staticmethod
     def test(self):
         sc = Scheduler()
         sc.getInstancesOrderByTime()
