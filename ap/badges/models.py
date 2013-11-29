aa58a01e02c989dbf91a367534f497c5dc89a6f2
Jonathan Tien <jonathan.tien@gmail.com>
2013-11-29 13:50:50 -0800

    basic badges model

---
+++ ap/badges/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+
+from terms.models import Term
+
+class Badge(models.model):
+    """
+    A training badge. There are different badges for trainees,
+    staff and so forth. Otherwise known as a profile picture.
+    """
+
+    BADGE_TYPES = (
+        ('T', 'Trainee'),
+        ('S', 'Staff'),
+    )
+
+
+
+    type = models.CharField(max_length=2, choices=BADGE_TYPES)
+    original = models.ImageField(upload_to='badges/')
