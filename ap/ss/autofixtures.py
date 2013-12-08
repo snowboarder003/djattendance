7009fa6501b25fe6b7916d1c11e9fb1087a08a20
Anatol Tsang <fermat200pg@gmail.com>
2013-12-07 16:08:38 -0800

    Added a generic name generator.

--- ap/ss/autofixtures.py
+++ ap/ss/autofixtures.py
@@ -14,6 +14,28 @@ $ django-admin.py loadtestdata accounts.User:50 accounts.TrainingAssistant:5 acc
 (note: generate Users before generating TAs and Trainees)
 """
 
+class AsianNameGenerator(generators.Generator):
+    """
+    Generates a random generic Asian name.
+    """
+
+    names = [
+        "Ahn", "Baek", "Bai", "Bang", "Bong", "Chai", "Chang", "Chao", "Chen",
+        "Chern", "Cheung", "Chi", "Chiang", "Chiu", "Choi", "Chou", "Chiu",
+        "Chu", "Chuah", "Chuang", "Co", "Ding", "Duong", "Feng", "Fok", "Han",
+        "Harm", "He", "Hsieh", "Hsiung", "Hsu", "Hu", "Hua", "Huang", "Hung",
+        "Hwang", "Jahng", "Jeng", "Jiao", "Jiang", "Jin", "Jou", "Jung", "Kan",
+        "Kao", "Kim", "Kor", "Kuo", "Lan", "Lee", "Leung", "Li", "Liang",
+        "Lie", "Lim", "Lin", "Liou", "Liu", "Loke", "Lu", "Luo", "Luor", "Mai",
+        "Mok", "Pan", "Park", "Peng", "Perng", "Po", "Qin", "Qu", "Ser",
+        "Shao", "Shi", "Tai", "Tak", "Tan", "Tien", "Tsai", "Tsang", "Ung",
+        "Wang", "Wei", "Wong", "Wu", "Xie", "Xu", "Xue", "Yan", "Yang", "Yao",
+        "Ye", "Yen", "Young", "Yu", "Zhan", "Zheng", "Zhou", "Zhu", "Zou"
+    ]
+
+    def generate(self):
+        return random.choice(self.names)
+
 
 class UserAutoFixture(AutoFixture):
     gender_ratios = [(generators.StaticGenerator("B"), 45),
@@ -21,8 +43,10 @@ class UserAutoFixture(AutoFixture):
     field_values = {
         'gender': generators.WeightedGenerator(choices=gender_ratios),
         'email': generators.EmailGenerator(static_domain='example.com'),
-        'firstname': generators.FirstNameGenerator(),
-        'lastname': generators.LastNameGenerator()
+        'firstname': AsianNameGenerator(),
+        'lastname': AsianNameGenerator(),
+        'middlename': AsianNameGenerator(),
+        'nickname': AsianNameGenerator()
     }
 
 register(User, UserAutoFixture)
