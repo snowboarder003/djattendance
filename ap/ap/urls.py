e669ccc49ce47fe3d859533823e9494872956ff1
Jonathan Tien <jonathan.tien@gmail.com>
2013-12-07 22:46:27 -0800

    added badges to URLconf

--- ap/ap/urls.py
+++ ap/ap/urls.py
@@ -17,6 +17,8 @@ urlpatterns = patterns('',
 
     url(r'^terms/', include('terms.urls', namespace="terms")),
     url(r'^dailybread/', include('dailybread.urls', namespace="dailybread")),
+    url(r'^badges/', include('badges.urls', namespace="badges")),
+
     # Examples:
     # url(r'^$', 'ap.views.home', name='home'),
     # url(r'^ap/', include('ap.foo.urls')),
