a97899752988ea1d1056bdf0f2c17311549435e3
Jonathan Tien <jonathan.tien@gmail.com>
2013-12-10 13:33:08 -0800

    added badge info fields

--- ap/badges/models.py
+++ ap/badges/models.py
@@ -17,6 +17,17 @@ class Badge(models.Model):
 
     type = models.CharField(max_length=2, choices=BADGE_TYPES)
     original = models.ImageField(upload_to=_image_upload_path)
+    # thumbnail
+    # badge_size
+
+    # badge information
+    # can be automatically populated from user account
+    firstname = models.CharField(max_length=50)
+    middlename = models.CharField(max_length=1)
+    lastname = models.CharField(max_length=50)
+    title = models.CharField(max_length=30)
+    locality = models.CharField(max_length=100)
+
 
     def get_upload_path(self, filename, term=Term.current_term()[0].code):
         path = "badges/"
