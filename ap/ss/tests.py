71f35db70cc10e429ac189071a85933e64827d3e
Anatol Tsang <fermat200pg@gmail.com>
2013-12-14 15:46:51 -0800

    Added a basic skeleton for printing generated trainee data.

--- ap/ss/tests.py
+++ ap/ss/tests.py
@@ -1,7 +1,7 @@
 from django.test import TestCase
 
-from accounts.models import User
-from ss.autofixtures import UserAutoFixture
+from accounts.models import User, Trainee
+from ss.autofixtures import TraineeAutoFixture, UserAutoFixture
 import sys
 
 
@@ -13,10 +13,15 @@ class AutofixtureTests(TestCase):
         """
         # Open this file for the testing output.
         sys.stdout = open('test-autofixture.txt', 'w')
-        fixture = UserAutoFixture(User)
-        users = fixture.create(25)
+        user_fixture = UserAutoFixture(User)
+        trainee_fixture = TraineeAutoFixture(Trainee)
+        users = user_fixture.create(25)
+        trainees = trainee_fixture.create(25)
         for user in users:
             print "******* USER " + user.email + " *******"
             print "NAME: " + user.lastname + ", " + user.firstname
             print "GENDER: " + user.gender
             print "DATE OF BIRTH: " + user.date_of_birth.strftime('%Y/%m/%d')
+        for trainee in trainees:
+            print "******* TRAINEE " + 'TRAINEE' + " *******"
+            print "TEAM: " + trainee.team
