f5d2daf77f2f52aa87320efb670bef491f0cb6e9
Jonathan Yao <jonyao.o@gmail.com>
2013-12-19 20:17:57 -0800

    updated the tests to include a test for table overflow from one table to the next

--- ap/mealSeating/tests.py
+++ ap/mealSeating/tests.py
@@ -8,20 +8,41 @@ from datetime import date
 
 from django.test import TestCase
 
-from accounts.models import User
-from accounts.models import Trainee
+from accounts.models import User, Trainee
 from mealSeating.models import Table
+from mealSeating.autofixtures import TraineeAutoFixture, UserAutoFixture
 
+
+def setup_trainees_autofixture():
+    """
+        This is a test case for using autofixtures in a test.
+    """
+    user_fixture, trainee_fixture = UserAutoFixture(User), \
+        TraineeAutoFixture(Trainee)
+    users, trainees = user_fixture.create(25), trainee_fixture.create(25)
 
 def setup_southeast_tables():
     """
         Creates all the table's in the southeast cafeteria
     """
     tables = []
-    tables.append(Table(name="SE-01", capacity=8, location="SE").save())
-    tables.append(Table(name="SE-02", capacity=8, location="SE").save())
-    tables.append(Table(name="SE-03", capacity=8, location="SE").save())
-    tables.append(Table(name="SE-04", capacity=8, location="SE").save())
+    tables.append(Table(name="SE-01", capacity=8, location="SE", genderType="B").save())
+    tables.append(Table(name="SE-02", capacity=8, location="SE", genderType="B").save())
+    tables.append(Table(name="SE-03", capacity=8, location="SE", genderType="B").save())
+    tables.append(Table(name="SE-04", capacity=8, location="SE", genderType="B").save())
+
+    return tables
+
+def setup_west_tables():
+    """
+        Creates all the table's in the west cafeteria
+    """
+    tables = []
+    tables.append(Table(name="W-01", capacity=8, location="W").save())
+    tables.append(Table(name="W-02", capacity=8, location="W").save())
+    tables.append(Table(name="W-03", capacity=8, location="W").save())
+    tables.append(Table(name="W-04", capacity=8, location="W").save())
+    tables.append(Table(name="W-05", capacity=8, location="W").save())
 
     return tables
 
@@ -55,14 +76,20 @@ def setup_marriedCouple():
 
 
 class TestTableCreation(TestCase):
-
-    def test_create_tables(self):
-        tables = setup_southeast_tables()
-        self.assertEqual(4, len(tables))
+    """
+        This test checks the tables are created according to the right locations.
+    """
+    def test_create_tables_for_location(self):
+        setup_southeast_tables()
+        setup_west_tables()
+        self.assertEqual(4, Table.objects.filter(location="SE").count())
+        self.assertEqual(5, Table.objects.filter(location="W").count())
 
 
 class TestMarriedCoupleFilter(TestCase):
-
+    '''
+        This test will check that the filter on the married couples is applied when seating the trainees
+    '''
     def test_create_seating_without_married_couples(self):
         setup_normalTrainee()
         setup_marriedCouple()
@@ -90,3 +117,35 @@ class TestMarriedCoupleFilter(TestCase):
 
         self.assertEqual(2, traineesToSeat.count())
         self.assertEqual(2, tablesAvailable.all()[0].seatedTrainees.count())
+
+class TestTableCapacity(TestCase):
+    '''
+        This test will test the seatTrainee() function in the Table module to
+        ensure that trainees will not be added beyond the capacity of the table.
+    '''
+    def test_seatTrainee_cannot_add_beyond_capacity(self):
+        setup_trainees_autofixture()
+        setup_southeast_tables()
+
+        table = Table.objects.filter(location="SE").all()[0]
+        traineesToSeat = Trainee.objects.filter(married=False)
+        
+        for trainee in traineesToSeat:
+            table.seatTrainee(trainee)
+
+        self.assertEqual(table.capacity, table.seatedTrainees.count())
+
+class TestTableOverflow(TestCase):
+    '''
+        This test will test that trainees being added to a group of tables, will be able to 
+        overflow from the first table to the next
+    '''
+    def test_seatTrainee_overflow(self):
+        setup_trainees_autofixture()
+        setup_southeast_tables()
+
+        traineesToSeat = Trainee.objects.filter(married=False).order_by('account__firstname')
+
+        Table.seatBrothers(traineesToSeat)
+        
+        self.assertLess(0, Table.objects.all()[1].getSeatedTrainees().count())
