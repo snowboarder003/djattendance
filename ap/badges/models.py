ac80e6dae08570fe12f1719f03f57880e3ea97e8
Jonathan Tien <jonathan.tien@gmail.com>
2013-12-20 15:25:45 -0800

    added fields for information that appears on badge. badge service may want to manually edit this information, so its better for it not be tied to other values. we will probably still seed it though

--- ap/badges/models.py
+++ ap/badges/models.py
@@ -15,18 +15,18 @@ class Badge(models.Model):
         ('S', 'Staff'),
     )
 
-    type = models.CharField(max_length=2, choices=BADGE_TYPES)
+    type = models.CharField(max_length=2, choices=BADGE_TYPES, default='T')
     original = models.ImageField(upload_to=_image_upload_path)
     # thumbnail
     # badge_size
 
     # badge information
     # can be automatically populated from user account
-    firstname = models.CharField(max_length=50)
-    middlename = models.CharField(max_length=1)
-    lastname = models.CharField(max_length=50)
-    title = models.CharField(max_length=30)
-    locality = models.CharField(max_length=100)
+    firstname = models.CharField(max_length=50, null=True, blank=True)
+    middlename = models.CharField(max_length=1, null=True, blank=True)
+    lastname = models.CharField(max_length=50, null=True, blank=True)
+    title = models.CharField(max_length=30, null=True, blank=True)
+    locality = models.CharField(max_length=100, null=True, blank=True)
 
 
     def get_upload_path(self, filename, term=Term.current_term()[0].code):
