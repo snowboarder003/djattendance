59ec3a6fba6bef612073952ed9cb305f7aeaf1c8
Jonathan Yao <jonyao.o@gmail.com>
2013-12-19 20:17:57 -0800

    updated the test cases to include setup for normal and married trainee, testing capability of adding trainees to table based on married couple filter

--- ap/mealSeating/tests.py
+++ ap/mealSeating/tests.py
@@ -4,8 +4,12 @@ when you run "manage.py test".
 
 Replace this with more appropriate tests for your application.
 """
+from datetime import date
 
 from django.test import TestCase
+
+from accounts.models import User
+from accounts.models import Trainee
 from mealSeating.models import Table
 
 
@@ -22,8 +26,67 @@ def setup_southeast_tables():
     return tables
 
 
+def setup_normalTrainee():
+    """
+        Creates normal trainee that can be added to the meal seating list
+    """
+    brother = User(email="normalBrother@gmail.com", username="normalBrother",
+                   firstname="David", is_active=True)
+    brother.save()
+    Trainee(account=brother, type="R", married=False,
+            date_begin=date.today()).save()
+
+
+def setup_marriedCouple():
+    """
+        Creates married couple which will be utlized in the following tests to
+        ensure they are not added to the meal seating list
+    """
+    brother = User(email="marriedBrother@gmail.com", username="marriedBrother",
+                   firstname="bob", is_active=True)
+    sister = User(email="marriedSister@gmail.com", username="marriedSister",
+                  firstname="merry", is_active=True)
+    brother.save()
+    sister.save()
+    traineeBro = Trainee(account=brother, type="R", married=True,
+                         date_begin=date.today()).save()
+    Trainee(account=sister, type="R", married=True, spouse=traineeBro,
+            date_begin=date.today()).save()
+
+
 class TestTableCreation(TestCase):
 
     def test_create_tables(self):
         tables = setup_southeast_tables()
         self.assertEqual(4, len(tables))
+
+
+class TestMarriedCoupleFilter(TestCase):
+
+    def test_create_seating_without_married_couples(self):
+        setup_normalTrainee()
+        setup_marriedCouple()
+        setup_southeast_tables()
+
+        traineesToSeat = Trainee.objects.filter(married=False)
+        tablesAvailable = Table.objects.filter(location="SE")
+
+        [tablesAvailable.all()[0].seatedTrainees.add(trainee)
+            for trainee in traineesToSeat]
+
+        self.assertEqual(1, tablesAvailable.all()[0].seatedTrainees.count())
+        self.assertEqual("David", tablesAvailable.all()[0].seatedTrainees.all()[0].account.get_full_name())
+
+    def test_create_seating_with_married_couples(self):
+        setup_normalTrainee()
+        setup_marriedCouple()
+        setup_southeast_tables()
+
+        traineesToSeat = Trainee.objects.filter(married=True)
+        tablesAvailable = Table.objects.filter(location="SE")
+
+        [tablesAvailable.all()[0].seatedTrainees.add(trainee)
+            for trainee in traineesToSeat]
+
+        self.assertEqual(2, traineesToSeat.count())
+        self.assertEqual(2, tablesAvailable.all()[0].seatedTrainees.count())
