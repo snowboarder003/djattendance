cb89bc8b65b1b2503bb3ee9a112e5eb70eb1959d
Jonathan Tien <jonathan.tien@gmail.com>
2013-12-24 23:34:45 -0800

    added basic badge assignment logic. also updated view name

--- ap/badges/views.py
+++ ap/badges/views.py
@@ -2,19 +2,32 @@ from django.http import HttpResponseRedirect
 from django.template import RequestContext
 from django.core.urlresolvers import reverse
 from django.shortcuts import render_to_response
+from django.db.models import Q
  
 from .forms import UploadBadgeForm
 from .models import Badge
+
+from accounts.models import User
  
 def home(request):
 
     return render_to_response('badge/index.html')
 
-def upload(request):
+def batch(request):
     if request.method == 'POST':
         b = Badge(type='T')
         b.original = request.FILES['file']
         b.save()
+
+        # grab the trainee name
+        last, first, ext = b.original.name.split('/')[-1].split('.')
+
+        user = User.objects.get(Q(is_active=True), 
+                                   Q(firstname__exact=first), 
+                                   Q(lastname__exact=last))
+
+        user.trainee
+
 
         return HttpResponseRedirect(reverse('badges:home'))
         
