fc71e50c93f8d75e108de554fcec88e194721a48
Jonathan Tien <jonathan.tien@gmail.com>
2013-12-29 04:22:15 -0800

    commented out some URLs for now, fixed view names

--- ap/badges/views.py
+++ ap/badges/views.py
@@ -9,9 +9,9 @@ from .models import Badge
 
 from accounts.models import User
  
-def home(request):
+def index(request):
 
-    return render_to_response('badge/index.html')
+    return render_to_response('badges/index.html')
 
 def batch(request):
     if request.method == 'POST':
@@ -20,16 +20,16 @@ def batch(request):
         b.save()
 
         # grab the trainee name
-        last, first, ext = b.original.name.split('/')[-1].split('.')
+        name = b.original.name.split('/')[-1].split('.')[0].split('_')
 
         user = User.objects.get(Q(is_active=True), 
-                                   Q(firstname__exact=first), 
-                                   Q(lastname__exact=last))
+                                Q(firstname__exact=name[1]), 
+                                Q(lastname__exact=name[0]))
 
-        user.trainee
+        user.trainee.badge = b
 
 
-        return HttpResponseRedirect(reverse('badges:home'))
+        return HttpResponseRedirect(reverse('badges:index'))
         
 
-    return render_to_response('badges/index.html', context_instance=RequestContext(request))
+    return render_to_response('badges/batch.html', context_instance=RequestContext(request))
