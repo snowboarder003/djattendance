3f9dd11851da710ceacadbf280e3f6b2f3a29806
Allen <allenwebbtx@gmail.com>
2014-01-14 00:19:15 -0600

    Improved the admin pages for houses

--- ap/houses/admin.py
+++ ap/houses/admin.py
@@ -1,5 +1,9 @@
 from django import forms
 from django.contrib import admin
+from django.utils.safestring import mark_safe
+from django.core.urlresolvers import reverse
+
+from accounts.models import Trainee
 
 from houses.models import House, Room, Bunk, MattressType, BedFrameType
 from aputils.models import Address
@@ -11,6 +15,16 @@ class HouseAdminForm(forms.ModelForm):
         model = House
 
 
+class RoomInline(admin.TabularInline):
+    model = Room
+    
+    def admin_link(self, instance):
+        url = reverse('admin:%s_%s_change' % (
+            instance._meta.app_label,  instance._meta.module_name),  args=[instance.pk] )
+        return mark_safe(u'<a href="{u}">Edit</a>'.format(u=url))
+
+    readonly_fields = ('admin_link',)
+
 class HouseAdmin(admin.ModelAdmin):
     form = HouseAdminForm
     list_display = (
@@ -19,12 +33,28 @@ class HouseAdmin(admin.ModelAdmin):
         'gender',
         'used',
     )
+    inlines = [ RoomInline ]
     ordering = ('used', 'gender', 'name', 'address',)
     search_fields = ['name']
+    
+
+class BunkInlineForm(forms.ModelForm):
+    trainee = forms.ModelChoiceField(queryset=Trainee.objects.filter(active=True).order_by('account__lastname','account__firstname'))
+    
+    class Meta:
+        model = Bunk
+
+class BunkInline(admin.StackedInline):
+    model = Bunk
+    form = BunkInlineForm 
+    readonly_fields = ('link',)
+    
+class RoomAdmin(admin.ModelAdmin):
+    inlines = [ BunkInline ]
 
 
 admin.site.register(House, HouseAdmin)
-admin.site.register(Room)
+admin.site.register(Room, RoomAdmin)
 admin.site.register(Bunk)
 admin.site.register(MattressType)
 admin.site.register(BedFrameType)
