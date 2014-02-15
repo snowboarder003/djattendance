76ec84a5bd704f4ee8f829d6f6085e8217e1e238
David Tien <david@DHTien-ThinkPad-X220.(none)>
2014-02-14 18:50:21 -0800

    changes in urls

--- ap/ap/urls.py
+++ ap/ap/urls.py
@@ -3,6 +3,7 @@ from django.contrib.auth.views import login, logout
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 import autofixture
 
 admin.autodiscover()
@@ -25,5 +26,7 @@ urlpatterns = patterns('',
 
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
-    (r’^static/(?P.*)$’, ‘django.views.static.serve’, {‘document_root’: settings.STATIC_ROOT}),
+    #(r’^static/(?P.*)$’, ‘django.views.static.serve’, {‘document_root’: settings.STATIC_ROOT}),
 )
+
+urlpatterns += staticfiles_urlpatterns()
