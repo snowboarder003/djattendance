c0c0f97e2bb90ad366e058867192ce47dc5f7904
Sarah Rumbley <srumbley@mit.edu>
2014-03-29 14:10:35 -0700

    removed commented out lines

--- ap/verse_parse/urls.py
+++ ap/verse_parse/urls.py
@@ -1,9 +1,6 @@
 from django.conf.urls import patterns, url
 from verse_parse import views
-#from verse_parse.preview import FileFormPreview
-#from verse_parse.forms import DisplayForm
 
 urlpatterns = patterns('',
 	url(r'^$', views.upload_file, name='verse_parse'),
-	#url(r'^post/$', FileFormPreview(DisplayForm)),
 )
