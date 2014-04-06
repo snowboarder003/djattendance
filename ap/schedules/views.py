c43729deac59e8d642a16d56b32039383b6edbe8
Jonathan Tien <jonathan.tien@gmail.com>
2014-04-06 11:07:57 -0700

    rudimentary CRUD for schedule models

--- ap/schedules/views.py
+++ ap/schedules/views.py
@@ -1,32 +1,48 @@
 from django.views import generic
+from django.core.urlresolvers import reverse_lazy
 from django.http import HttpResponse
 from django.template import RequestContext 
+from django.forms.models import modelform_factory
+
 from .models import Schedule, ScheduleTemplate, Event, EventGroup
+from django.contrib.admin.widgets import AdminDateWidget, AdminTimeWidget
 
 
+class EventCreate(generic.CreateView):
+    model = Event
+    fields = ['name', 'code', 'description', 'group', 'classs', 'type', 'date', 'start', 'end']
+    template_name = 'schedules/event_create.html'
+    form_class =  modelform_factory(Event,
+                                    widgets = { "date": AdminDateWidget() })
 
 
-class ScheduleCreateView(generic.CreateView):
+class EventDetail(generic.DetailView):
+    model = Event
+    context_object_name = "event"
+
+
+class EventUpdate(generic.UpdateView):
+    model = Event
+    template_name = 'schedules/event_update.html'
+
+
+class EventDelete(generic.DeleteView):
+    model = Event
+    success_url = reverse_lazy('event-create')
+    
+
+class ScheduleCreate(generic.CreateView):
     model = ScheduleTemplate
     template_name = 'schedules/new_schedule.html'
     fields = ['name']
 
-class ScheduleListView(generic.ListView):
-	model = ScheduleTemplate
-	template_name = 'schedules/list.html'
-	context_object_name = 'list_of_schedules'
 
-	def get_queryset(self):
-		return ScheduleTemplate.objects.all
+class ScheduleList(generic.ListView):
+    model = ScheduleTemplate
+    template_name = 'schedules/list.html'
+    context_object_name = 'list_of_schedules'
 
-# @login_required
-def update_event(request):
-	context = RequestContext(request)
-	title = ''
-	if request.method == 'GET':
-		print request
-		print request.GET
+    def get_queryset(self):
+        return ScheduleTemplate.objects.all
 
-	# Do the update on the model here...
 
-	return HttpResponse(request.GET['title'] + ' returned from view.')
