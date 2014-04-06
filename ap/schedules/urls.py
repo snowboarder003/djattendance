ef427deea8e7db3d51df5158dddfa9239f9a160b
Jonathan Tien <jonathan.tien@gmail.com>
2014-04-06 11:02:10 -0700

    cleaned up some schedule urls because it was causing conflicts with event urls

--- ap/schedules/urls.py
+++ ap/schedules/urls.py
@@ -4,7 +4,10 @@ from django.conf import settings
 from schedules import views
 
 urlpatterns = patterns('',
-    url(r'list/$', views.ScheduleListView.as_view(), name='list'),
-    url(r'create/$', views.ScheduleCreateView.as_view(), name='create'),
-    url(r'update_event/$', views.update_event, name='update_event'),
+    url(r'schedule/list/$', views.ScheduleList.as_view(), name='schedule-list'),
+    url(r'schedule/create/$', views.ScheduleCreate.as_view(), name='schedule-create'),
+    url(r'event/create/$', views.EventCreate.as_view(), name='event-create'),
+    url(r'event/(?P<pk>\d+)/$', views.EventDetail.as_view(), name='event-detail'),
+    url(r'event/(?P<pk>\d+)/update/$', views.EventUpdate.as_view(), name='event-update'),
+    url(r'event/(?P<pk>\d+)/delete/$', views.EventDelete.as_view(), name='event-delete'),
 )
