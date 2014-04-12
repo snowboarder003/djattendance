2048eef729e959ec5a2c440a1d155b80057da711
Jonathan Tien <jonathan.tien@gmail.com>
2014-04-12 13:09:21 -0700

    basic working schedule/calendar view

--- ap/schedules/views.py
+++ ap/schedules/views.py
@@ -1,12 +1,39 @@
 from django.views import generic
 from django.core.urlresolvers import reverse_lazy
 from django.http import HttpResponse
-from django.template import RequestContext 
+from django.template import RequestContext
 from django.forms.models import modelform_factory
+from django.contrib.admin.widgets import AdminDateWidget
 
 from .models import Schedule, ScheduleTemplate, Event, EventGroup
-from django.contrib.admin.widgets import AdminDateWidget, AdminTimeWidget
+from terms.models import Term
 
+
+class ScheduleDetail(generic.DetailView):
+    model = Schedule
+    template_name = 'schedules/schedule_detail.html'
+    context_object_name = 'schedule'
+
+    def get_queryset(self):
+        if not self.args:
+            self.user = self.request.user
+        else:
+            self.user = get_object_or_404(User, pk=self.args[0])
+        return Schedule.objects.filter(trainee=self.user.trainee).filter(term=Term.current_term())
+
+class ScheduleCreate(generic.CreateView):
+    model = ScheduleTemplate
+    template_name = 'schedules/new_schedule.html'
+    fields = ['name']
+
+
+class ScheduleList(generic.ListView):
+    model = ScheduleTemplate
+    template_name = 'schedules/list.html'
+    context_object_name = 'list_of_schedules'
+
+    def get_queryset(self):
+        return ScheduleTemplate.objects.all
 
 class EventCreate(generic.CreateView):
     model = Event
@@ -30,20 +57,3 @@ class EventUpdate(generic.UpdateView):
 class EventDelete(generic.DeleteView):
     model = Event
     success_url = reverse_lazy('event-create')
-    
-
-class ScheduleCreate(generic.CreateView):
-    model = ScheduleTemplate
-    template_name = 'schedules/new_schedule.html'
-    fields = ['name']
-
-
-class ScheduleList(generic.ListView):
-    model = ScheduleTemplate
-    template_name = 'schedules/list.html'
-    context_object_name = 'list_of_schedules'
-
-    def get_queryset(self):
-        return ScheduleTemplate.objects.all
-
-
