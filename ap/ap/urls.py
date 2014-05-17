6015e4929dc5a05391b3d830fcc73a06bb13d1f5
Sarah Rumbley <srumbley@mit.edu>
2014-05-17 14:15:12 -0700

    basic install of django-autocomplete-light

--- ap/ap/urls.py
+++ ap/ap/urls.py
@@ -1,6 +1,10 @@
 from django.conf.urls import patterns, include, url
 from django.contrib.auth.views import login, logout
 
+import autocomplete_light
+# import every app/autocomplete_light_registry.py
+autocomplete_light.autodiscover()
+
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
 import autofixture
@@ -27,5 +31,8 @@ urlpatterns = patterns('',
 
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
+
+    # django-autocomplete-light view and staff debug view
+    url(r'^autocomplete/', include('autocomplete_light.urls')),
 )
 
