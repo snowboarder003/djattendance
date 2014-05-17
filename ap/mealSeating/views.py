b13a54ce91bec19f4e3f1827878bf25e930492d7
Jonathan Yao <jonyao.o@gmail.com>
2014-05-17 16:16:03 -0700

    can view meal seating arrangement for brothers and sisters

--- ap/mealSeating/views.py
+++ ap/mealSeating/views.py
@@ -1,1 +1,51 @@
 # Create your views here.
+from django.http import HttpResponse
+from django.views.generic import ListView
+
+from django.shortcuts import render
+
+from django_tables2 import RequestConfig
+from django_tables2 import tables
+
+from .models import Table
+from .tables import TablesTable
+from accounts.models import User, Trainee, Profile
+
+# def seating(request):
+#     return HttpResponse("Hello, world. You're at the meal seating page.")
+def brothertables(request):
+
+    trainees = Trainee.objects.all().filter(account__gender__contains="B").order_by('account__firstname')[:50]
+    tablesList = Table.objects.all()
+
+    myList = Table.seatTables(trainees, tablesList)
+
+    splitValue = len(myList) / 3
+
+    myList1 = myList[:splitValue]
+    myList2 = myList[splitValue:splitValue*2]
+    myList3 = myList[splitValue*2:]
+
+    return render(request, 'detail.html', {'myList1': myList1 , 'myList2': myList2, 'myList3' : myList3, 'today' : today})
+
+
+def sistertables(request):
+
+    trainees = Trainee.objects.all().filter(account__gender__contains="S").order_by('account__firstname')[:50]
+    tablesList = Table.objects.all()
+
+    myList = Table.seatTables(trainees, tablesList)
+
+
+    splitValue = len(myList) / 3
+
+    myList1 = myList[:splitValue]
+    myList2 = myList[splitValue:splitValue*2]
+    myList3 = myList[splitValue*2:]
+
+    return render(request, 'detail.html', {'myList1': myList1 , 'myList2': myList2, 'myList3' : myList3})
+
+
+# class ViewList(ListView):
+#     model = Table
+#     context_object_name = 'table'
