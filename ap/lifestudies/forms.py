4be3dc5d0d19c7940a187282163cf197a1aacfc4
Dennis Lu <attendanceproject@Shao-Chengs-MacBook-Air.local>
2014-05-24 15:28:11 -0700

    added LifeStudy from in forms.py

--- ap/lifestudies/forms.py
+++ ap/lifestudies/forms.py
@@ -1,6 +1,16 @@
 from django import forms
 from lifestudies.models import LifeStudy, Summary
 
+class NewLifeStudyForm(forms.ModelFrom)
+    class Meta:
+        model = LifeStudy
+    def save(self, commit=True):
+        lifestudy = super(NewLifeStudyForm, self).save(commit=False)
+        if commit:
+            lifestudy.save()
+        return lifestudy
+
+
 class NewSummaryForm(forms.ModelForm):
     class Meta:
         model = Summary
@@ -9,6 +19,8 @@ class NewSummaryForm(forms.ModelForm):
         if commit:
             summary.save()
         return summary
+
+
 
 # class NewSyllabusForm(forms.ModelForm):
     
