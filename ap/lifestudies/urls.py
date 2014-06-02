7bcf3da0c83a55bcc363af33aaf34e48109aca54
Dennis Lu <attendanceproject@Shao-Chengs-MacBook-Air.local>
2014-06-02 16:08:37 -0700

    each pk name group takes more than 1 or more digit with 'd+' instead of 'd'

--- ap/lifestudies/urls.py
+++ ap/lifestudies/urls.py
@@ -4,10 +4,10 @@ from lifestudies.views import LifeStudyListView, LifeStudyCreateView, LifeStudyD
 
 urlpatterns = patterns('',
     url(r'^$', LifeStudyListView.as_view(), name='lifestudy-list'),
-    url(r'^(?P<pk>\d)/create_summary$', SummaryCreateView.as_view(), name='summary-create'),
-    url(r'^(?P<pk>\d)/detail_summary$', SummaryDetailView.as_view(), name='summary-detail'),
+    url(r'^(?P<pk>\d+)/create_summary$', SummaryCreateView.as_view(), name='summary-create'),
+    url(r'^(?P<pk>\d+)/detail_summary$', SummaryDetailView.as_view(), name='summary-detail'),
     url(r'^create_lifestudy$', LifeStudyCreateView.as_view(), name='lifestudy-create'),
-    url(r'^(?P<pk>\d)/detail_lifestudy$', LifeStudyDetailView.as_view(), name='lifestudy-detail'),
+    url(r'^(?P<pk>\d+)/detail_lifestudy$', LifeStudyDetailView.as_view(), name='lifestudy-detail'),
 )
 
 
