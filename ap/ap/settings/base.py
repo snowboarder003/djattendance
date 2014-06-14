62f343210b5dd9b0d409e996fa93a059e19302ff
ysbecca <ysbecca@gmail.com>
2014-06-14 12:47:49 -0700

    moving api to the leaveslip branch

--- ap/ap/settings/base.py
+++ ap/ap/settings/base.py
@@ -143,6 +143,7 @@ INSTALLED_APPS = (
     'schedules',
     'attendance',
     'dailybread',  # daily nourishment
+    'leaveslip_api',
 )
 
 # A sample logging configuration. The only tangible logging
--- ap/ap/urls.py
+++ ap/ap/urls.py
@@ -1,9 +1,18 @@
 from django.conf.urls import patterns, include, url
 from django.contrib.auth.views import login, logout
 
+import autocomplete_light
+# import every app/autocomplete_light_registry.py
+import accounts.autocomplete_light_registry
+autocomplete_light.autodiscover()
+
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
 import autofixture
+
+from tastypie.api import Api
+from leaveslip_api.resources import IndividualSlipResource, GroupSlipResource, MealOutSlipResource, NightOutSlipResource
+
 
 admin.autodiscover()
 autofixture.autodiscover()
@@ -18,6 +27,7 @@ urlpatterns = patterns('',
     url(r'^dailybread/', include('dailybread.urls', namespace="dailybread")),
     url(r'^schedules/', include('schedules.urls', namespace="schedules")),
     url(r'^leaveslips/', include('leaveslips.urls', namespace="leaveslips")),
+	url(r'^attendance/', include('attendance.urls', namespace="attendance")),
 
     # Examples:
     # url(r'^$', 'ap.views.home', name='home'),
@@ -28,5 +38,13 @@ urlpatterns = patterns('',
 
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
-)
 
+    # django-autocomplete-light view and staff debug view
+    url(r'^autocomplete/', include('autocomplete_light.urls')),
+
+    # leaveslips apis
+    url(r'^leaveslip-api/', include(IndividualSlipResource().urls)),
+    url(r'^leaveslip-api/', include(GroupSlipResource().urls)),
+    url(r'^leaveslip-api/', include(MealOutSlipResource().urls)),
+    url(r'^leaveslip-api/', include(NightOutSlipResource().urls)),
+)
