a57ac3b74dd508acaf33492bc1c1cc3e53ad56f4
Dennis Lu <attendanceproject@Shao-Chengs-MacBook-Air.local>
2014-06-14 16:02:26 -0700

    cleaned up some comments in forms.py

--- ap/lifestudies/forms.py
+++ ap/lifestudies/forms.py
@@ -40,23 +40,6 @@ class HouseLifeStudyForm(forms.ModelForm):
         model = LifeStudy
         exclude = ('trainee',)
     House = forms.ModelChoiceField(House.objects)
-    
 
-"""
-class ItemForm(ModelForm):
-    def __init__(self, *args, **kwargs):
-        super(ItemForm, self).__init__(*args, **kwargs)
-        instance = getattr(self, 'instance', None)
-        if instance and instance.pk:
-            self.fields['sku'].widget.attrs['readonly'] = True
-
-    def clean_sku(self):
-        instance = getattr(self, 'instance', None)
-        if instance and instance.pk:
-            return instance.sku
-        else:
-            return self.cleaned_data['sku']
-
-"""
 
 
