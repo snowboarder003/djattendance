206bfbc7b1bafdae5ab19787118d4234edbd231c
Jonathan Yao <jonyao.o@gmail.com>
2014-06-21 13:30:03 -0700

    implementation change
    changed implementation from dictionary to list of dictionary

--- ap/mealSeating/models.py
+++ ap/mealSeating/models.py
@@ -9,6 +9,38 @@ brothers and sisters, and also for short-termers.
 """
 
 
+
+#dict ((o.lastname + ", " + o.firstname, o.pk) for o in User.objecs.all())
+# 
+#
+# for o in sortedList:
+#   mydict[o.account.lastname + ", " + o.account.firstname] = o.pk
+#
+# <ul>
+# {% for key, value in choices.items %} 
+#   <li>{{key}} - {{value}}</li>
+#  {% endfor %}
+# </ul>
+
+# def seatinglist(genderlist, gender):
+#     tables = Table.objects.filter(genderType = gender)
+#     traineenum = 1
+#     tablenum = 0
+#     mealSeating = {}
+#     totalcapacity = 0
+#     for x in Table.objects.all().filter(genderType = gender).values("capacity"):
+#         totalcapacity += x["capacity"]
+    # if (len(genderlist) > totalcapacity):
+#         print "cannot seat " + traineenum + " trainees. Current capacity is: " + capacity
+    # else:
+    #     for trainee in genderlist:    
+    #         if ( traineenum == tables[tablenum].capacity):
+    #             tablenum += 1
+    #             traineenum = 0
+    #         mealseating[trainee.lastname + ", " + trainee.firstname] = tables[tablenum]
+    #         traineenum += 1
+    #     return mealSeating    
+
 class Table(models.Model):
 
     name = models.CharField(max_length=10)
@@ -24,48 +56,48 @@ class Table(models.Model):
         ('S', 'Sister'),
     )
 
-    seatedTrainees = models.ManyToManyField(Trainee, blank=True, null=True)
+    # seatedTrainees = models.ManyToManyField(Trainee, blank=True, null=True)
     location = models.CharField(max_length=2, choices=LOCATIONS)
     genderType = models.CharField(max_length=1, choices=GENDERS)
 
 
-    def getEmptySeats(self):
-        return self.capacity - self.seatedTrainees.count()
+    # def getEmptySeats(self):
+    #     return self.capacity - self.seatedTrainees.count()
 
-    def isFull(self):
-        return self.getEmptySeats() == 0
+    # def isFull(self):
+    #     return self.getEmptySeats() == 0
 
-    def clearTables(self):
-        self.seatedTrainees = []
-        return True
+    # def clearTables(self):
+    #     self.seatedTrainees = []
+    #     return True
 
-    def seatTrainee(self, trainee):
-        if(self.getEmptySeats() != 0):
-            self.seatedTrainees.add(trainee)
-            return True
-        else:
-            return False
+    # def seatTrainee(self, trainee):
+    #     if(self.getEmptySeats() != 0):
+    #         self.seatedTrainees.add(trainee)
+    #         return True
+    #     else:
+    #         return False
 
-    def clearTrainees(self):
-        for tables in Table.objects.all():
-            tables.seatedTrainees.clear()
-        return True
+    # def clearTrainees(self):
+    #     for tables in Table.objects.all():
+    #         tables.seatedTrainees.clear()
+    #     return True
 
-    def splitTablesByGender(self, tables):
-        brothersTables = []
-        sistersTables = []
-        tableSplit = [brothersTables, sistersTables]
+    # def splitTablesByGender(self, tables):
+    #     brothersTables = []
+    #     sistersTables = []
+    #     tableSplit = [brothersTables, sistersTables]
 
-        for table in tables:
-            if table.genderType == "B":
-                brothersTables.append(table)
-            else:
-                sistersTables.append(table)
+    #     for table in tables:
+    #         if table.genderType == "B":
+    #             brothersTables.append(table)
+    #         else:
+    #             sistersTables.append(table)
 
-        return tableSplit
+    #     return tableSplit
 
-    def getSeatedTrainees(self):
-        return self.seatedTrainees.all()
+    # def getSeatedTrainees(self):
+    #     return self.seatedTrainees.all()
 
     def getCapacity(self):
         return self.capacity
@@ -74,41 +106,65 @@ class Table(models.Model):
         return self.name
     
     @staticmethod
-    def seatTables(traineeList, tableList):
-        tables = Table().splitTablesByGender(tableList)
+    def seatinglist(genderlist, gender):
+        tables = Table.objects.filter(genderType = gender)
+        traineenum = 0
+        tablenum = 0
+        totalcapacity = 0
+        meal_list = []
+        for x in Table.objects.all().filter(genderType = gender).values("capacity"):
+            totalcapacity += x["capacity"]
+        if (len(genderlist) > totalcapacity):
+            print "cannot seat " + traineenum + " trainees. Current capacity is: " + capacity
+        else:
+            for trainee in genderlist:    
+                mealseating = {}
+                if ( traineenum == tables[tablenum].capacity):
+                    tablenum += 1
+                    traineenum = 0
+                mealseating["first_name"] = trainee.firstname
+                mealseating["last_name"] = trainee.lastname
+                mealseating["table"] = tables[tablenum]
+                meal_list.append(mealseating)
+                traineenum += 1
+            return meal_list    
 
-        Table().clearTrainees()
+    # @staticmethod
+    # def seatTables(traineeList, tableList):
+    #     tables = Table().splitTablesByGender(tableList)
 
-        tableToAddTo = 0
-        seatingList = []
-        brothers = 0
-        sisters = 1
-        broCounter = 0
-        sisCounter = 0
-        firstPass = True
+    #     Table().clearTrainees()
 
-        for trainee in traineeList:
-            if trainee.account.gender == "B":
-                gender = brothers
-                tableToAddTo = broCounter
-            else:
-                gender = sisters 
-                tableToAddTo = sisCounter
+    #     tableToAddTo = 0
+    #     seatingList = []
+    #     brothers = 0
+    #     sisters = 1
+    #     broCounter = 0
+    #     sisCounter = 0
+    #     firstPass = True
 
-            if(firstPass is False):
-                if tables[gender][tableToAddTo].isFull() & (tableToAddTo <= (len(tables[gender])-1)):
-                    if trainee.account.gender == "B":
-                        broCounter += 1
-                        tableToAddTo = broCounter
-                    else:
-                        sisCounter += 1
-                        tableToAddTo = sisCounter
-                    firstPass = True
+    #     for trainee in traineeList:
+    #         if trainee.account.gender == "B":
+    #             gender = brothers
+    #             tableToAddTo = broCounter
+    #         else:
+    #             gender = sisters 
+    #             tableToAddTo = sisCounter
 
-            tables[gender][tableToAddTo].seatTrainee(trainee)
-            entry = []
-            entry.append([trainee.account.get_full_name(),tables[gender][tableToAddTo].name])
-            seatingList.append(entry)
-            firstPass = False
+    #         if(firstPass is False):
+    #             if tables[gender][tableToAddTo].isFull() & (tableToAddTo <= (len(tables[gender])-1)):
+    #                 if trainee.account.gender == "B":
+    #                     broCounter += 1
+    #                     tableToAddTo = broCounter
+    #                 else:
+    #                     sisCounter += 1
+    #                     tableToAddTo = sisCounter
+    #                 firstPass = True
 
-        return seatingList
+    #         tables[gender][tableToAddTo].seatTrainee(trainee)
+    #         entry = []
+    #         entry.append([trainee.account.get_full_name(),tables[gender][tableToAddTo].name])
+    #         seatingList.append(entry)
+    #         firstPass = False
+
+    #     return seatingList
--- ap/mealSeating/views.py
+++ ap/mealSeating/views.py
@@ -14,37 +14,48 @@ from accounts.models import User, Trainee, Profile
 
 # def seating(request):
 #     return HttpResponse("Hello, world. You're at the meal seating page.")
-def brothertables(request):
 
-    trainees = Trainee.objects.all().filter(account__gender__contains="B").order_by('account__firstname')[:50]
+def seattables(request):
+    trainees = User.objects.all().filter(gender="B").order_by('lastname')[:50]
     tablesList = Table.objects.all()
 
-    myList = Table.seatTables(trainees, tablesList)
-
-    splitValue = len(myList) / 3
-
-    myList1 = myList[:splitValue]
-    myList2 = myList[splitValue:splitValue*2]
-    myList3 = myList[splitValue*2:]
-
-    return render(request, 'detail.html', {'myList1': myList1 , 'myList2': myList2, 'myList3' : myList3})
+    mydict = Table.seatinglist(trainees, "B")
 
 
-def sistertables(request):
 
-    trainees = Trainee.objects.all().filter(account__gender__contains="S").order_by('account__firstname')[:50]
-    tablesList = Table.objects.all()
+    return render(request, 'detail.html', {'mydict' : mydict})
 
-    myList = Table.seatTables(trainees, tablesList)
+# def brothertables(request):
+
+#     trainees = Trainee.objects.all().filter(account__gender__contains="B").order_by('account__firstname')[:50]
+#     tablesList = Table.objects.all()
+
+#     myList = Table.seatTables(trainees, tablesList)
+
+#     splitValue = len(myList) / 3
+
+#     myList1 = myList[:splitValue]
+#     myList2 = myList[splitValue:splitValue*2]
+#     myList3 = myList[splitValue*2:]
+
+#     return render(request, 'detail.html', {'myList1': myList1 , 'myList2': myList2, 'myList3' : myList3})
 
 
-    splitValue = len(myList) / 3
+# def sistertables(request):
 
-    myList1 = myList[:splitValue]
-    myList2 = myList[splitValue:splitValue*2]
-    myList3 = myList[splitValue*2:]
+#     trainees = Trainee.objects.all().filter(account__gender__contains="S").order_by('account__firstname')[:50]
+#     tablesList = Table.objects.all()
 
-    return render(request, 'detail.html', {'myList1': myList1 , 'myList2': myList2, 'myList3' : myList3})
+#     myList = Table.seatTables(trainees, tablesList)
+
+
+#     splitValue = len(myList) / 3
+
+#     myList1 = myList[:splitValue]
+#     myList2 = myList[splitValue:splitValue*2]
+#     myList3 = myList[splitValue*2:]
+
+#     return render(request, 'detail.html', {'myList1': myList1 , 'myList2': myList2, 'myList3' : myList3})
 
 
 # class ViewList(ListView):
