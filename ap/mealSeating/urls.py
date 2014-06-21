50e1a3ade1ce4c2c75d58a3b841ba42634f3234f
Jonathan Yao <jonyao.o@gmail.com>
2014-06-21 13:55:32 -0700

    fixed url for meal seating
    now takes gender variable for the specific meal seating list to return,
    whether brother or sister list.

--- ap/mealSeating/urls.py
+++ ap/mealSeating/urls.py
@@ -4,8 +4,8 @@ from mealSeating import views
 
 urlpatterns = patterns('',
     # url(r'^$', views.seating, name='detail'),
-    url(r'viewList/brothers/$',
-        views.brothertables),
-    url(r'viewList/sisters/$',
-        views.sistertables)
+    url(r'viewList/(?P<gender>[A-Z]{1})/$',
+        views.seattables),
+    # url(r'viewList/sisters/$',
+    #      views.seattables)
 )
