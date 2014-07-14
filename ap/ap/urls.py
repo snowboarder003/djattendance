0ab64c24ef1e416d0f72ec0e5694408a92ef6045
Jonathan Tien <jonathan.tien@gmail.com>
2014-07-14 15:28:46 -0700

    removed urls. reformatted urlconf

--- ap/ap/urls.py
+++ ap/ap/urls.py
@@ -11,24 +11,14 @@ admin.autodiscover()
 autofixture.autodiscover()
 
 urlpatterns = patterns('',
+    url(r'^$', 'ap.views.home'),
+
 	url(r'^accounts/login/$', login),
 	url(r'^accounts/logout/$', logout),
-    url(r'^$', 'ap.views.home'),
-    url(r'^base_example/$', 'ap.views.base_example'),
-
     url(r'^terms/', include('terms.urls', namespace="terms")),
     url(r'^dailybread/', include('dailybread.urls', namespace="dailybread")),
-    # Examples:
-    # url(r'^$', 'ap.views.home', name='home'),
-    # url(r'^ap/', include('ap.foo.urls')),
 
-    # Uncomment the admin/doc line below to enable admin documentation:
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
-    
-    
-    url(r'^houses/', include(houses_urls)),
 )
 
--- ap/houses/urls.py
+++ ap/houses/urls.py
@@ -1,5 +1,1 @@
-from django.conf.urls import patterns, include, url
 
-urlpatterns = patterns('',
-    url(r'^(?P<house__pk>[^/]{0,50})', 'houses.views.bunk_selector'),
-)
