cb367a6ba659e8b5798d1ac211051ec4daea3ad1
Jonathan Tien <jonathan.tien@gmail.com>
2014-07-16 12:09:33 -0700

    removed autocomplete_light and references

--- ap/ap/settings/base.py
+++ ap/ap/settings/base.py
@@ -127,7 +127,6 @@ INSTALLED_APPS = (
     'django.contrib.admin',
     'django.contrib.admindocs',
     # third-party modules
-    'autocomplete_light', # django autocomplete widgets
     'autofixture', # easily generate dummy/test data
     'bootstrap3', # easy-to-use bootstrap integration
     'bootstrap3_datetime', # datetime picker widget
--- ap/schedules/forms.py
+++ ap/schedules/forms.py
@@ -8,8 +8,7 @@ from accounts.models import Trainee
 
 
 class EventForm(forms.ModelForm):
-    trainees = forms.ModelMultipleChoiceField(Trainee.objects.all(), 
-            widget = autocomplete_light.MultipleChoiceWidget('TraineeAutocomplete'),
+    trainees = forms.ModelMultipleChoiceField(Trainee.objects.all(),
             required = False)
 
     class Meta:
@@ -18,16 +17,3 @@ class EventForm(forms.ModelForm):
         widgets = { 'start': DateTimePicker(options={'format': 'MM/DD/YYYY HH:mm'}),
                     'end': DateTimePicker(options={'format': 'MM/DD/YYYY HH:mm'}) }
 
-    # def clean_start(self):
-    # 	data = self.cleaned_data['start']
-    # 	if data[-2:] == 'AM':
-    # 		return data[:-3]
-    # 	elif data[-2:] == 'PM':
-    # 		if len(data) == 18: # hour has one digit
-    # 			hour = int(data[11]) + 12
-    # 		elif len(data) == 19: # hour has two digits
-    # 			hour = int(data[11:13])
-    # 		else:
-    # 			raise forms.ValidationError('Please enter date in format "MM/DD/YYYY HH:MM A/PM".')
-    # 		hour += 12
-    # 		return data[:11] + str(hour) + data[-3:]
