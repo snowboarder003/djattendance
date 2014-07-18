f32ea0c52d4c7ee41de78bf53c98edd029144c59
Spencer Ying <lifedispenser@gmail.com>
2014-07-18 15:57:17 -0700

    Terms no longer can modify name or code.

--- ap/terms/forms.py
+++ ap/terms/forms.py
@@ -17,16 +17,9 @@ class NewTermForm(forms.ModelForm):
         data = self.cleaned_data
         term = super(NewTermForm, self).save(commit=False)
         term.season = data['season']
-        term.name = data['season']
-        term.code = term.name[:2]
         term.start = data['start']
         term.end = data['end']
 
-        if term.start.year == term.end.year:
-            term.name += ' ' + str(term.start.year)
-            term.code += str(term.start.year)[2:]
-##        else:
-            # error
         ## check if term code has duplicate??
         
         if commit:
