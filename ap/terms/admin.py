c509655d13d8dfbf0dea899c1c83206d628dfe1e
Jonathan Tien <jonathan.tien@gmail.com>
2014-07-18 16:09:26 -0700

    removed outdated term form, fixes admin page

--- ap/terms/admin.py
+++ ap/terms/admin.py
@@ -1,9 +1,7 @@
 from django.contrib import admin
 from terms.models import Term
-from terms.forms import NewTermForm
 
 class TermAdmin(admin.ModelAdmin):
     ordering = ['-start']
-    form = NewTermForm
 
 admin.site.register(Term, TermAdmin)
