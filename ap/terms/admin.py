6bdde2abbf9416cde54c51c433baf4c173c70f37
Jonathan Tien <jonathan.tien@gmail.com>
2014-07-18 16:50:10 -0700

    removed unused terms code

--- ap/terms/admin.py
+++ ap/terms/admin.py
@@ -3,7 +3,6 @@ from terms.models import Term
 from terms.forms import NewTermForm
 
 class TermAdmin(admin.ModelAdmin):
-    ordering = ['-start']
-    form = NewTermForm
+    ordering = ['start']
 
 admin.site.register(Term, TermAdmin)
