d7f2925db41369c21c85fdff8e94b73917cacd0e
Dennis Lu <ludennis@hotmail.com>
2014-07-21 17:41:10 -0700

    added namespacing to lifestudies

--- ap/accounts/urls.py
+++ ap/accounts/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
-from django.contrib.auth.views import password_change, password_change_done
+from django.contrib.auth import views as auth_views
 
 from . import views
 
@@ -8,11 +8,13 @@ urlpatterns = patterns(
     '',
     url(regex=r'^(?P<pk>\d+)$', view=views.UserDetailView.as_view(), name='user_detail'),
     url(regex=r'^update/(?P<pk>\d+)$', view=views.UserUpdateView.as_view(), name='user_update'),
-    url(regex=r'^update/email/(?P<pk>\d+)$', view=view.sEmailUpdateView.as_view(), name='email_change'),
-    url(regex=r'^update/password$', view=password_change,
-        {'template_name': 'accounts/password_change_form.html'},
+    url(regex=r'^email/update/(?P<pk>\d+)$', view=views.EmailUpdateView.as_view(), name='email_change'),
+    url(regex=r'^password/change$', view=auth_views.password_change,
+        kwargs={'template_name': 'accounts/password_change_form.html',
+                'current_app': 'accounts'},
         name='password_change'),
-    url(regex=r'^update/password/success$', view=password_change_done,
-        {'template_name': 'accounts/password_change_done.html'},
+    url(regex=r'^password/change/done$', view=auth_views.password_change_done,
+        kwargs={'template_name': 'accounts/password_change_done.html',
+                'current_app': 'accounts'},
         name='password_change_done'),
 )
--- ap/lifestudies/urls.py
+++ ap/lifestudies/urls.py
@@ -1,26 +1,23 @@
 from django.conf.urls import patterns, url
-from .views import DisciplineListView, DisciplineCreateView, \
-    DisciplineDetailView, SummaryCreateView, SummaryUpdateView, \
-    SummaryApproveView, DisciplineReportView, CreateHouseDiscipline, \
-    AttendanceAssign
+from . import views
 
 urlpatterns = patterns(
     '',
-    url(r'^$', DisciplineListView.as_view(), name='discipline_list'),
-    url(r'^discipline-report$', DisciplineReportView.as_view(),
+    url(r'^$', views.DisciplineListView.as_view(), name='discipline_list'),
+    url(r'^discipline-report$', views.DisciplineReportView.as_view(),
         name='discipline_report'),
-    url(r'^(?P<pk>\d+)/create-summary$', SummaryCreateView.as_view(),
+    url(r'^(?P<pk>\d+)/create-summary$', views.SummaryCreateView.as_view(),
         name='summary_create'),
-    url(r'^(?P<pk>\d+)/detail-summary$', SummaryUpdateView.as_view(),
+    url(r'^(?P<pk>\d+)/detail-summary$', views.SummaryUpdateView.as_view(),
         name='summary_detail'),
-    url(r'^(?P<pk>\d+)/approve-summary$', SummaryApproveView.as_view(),
+    url(r'^(?P<pk>\d+)/approve-summary$', views.SummaryApproveView.as_view(),
         name='summary_approve'),
-    url(r'^create_discipline-house$', CreateHouseDiscipline.as_view(),
+    url(r'^create_discipline-house$', views.CreateHouseDiscipline.as_view(),
         name='discipline_house'),
-    url(r'^create-discipline$', DisciplineCreateView.as_view(),
+    url(r'^create-discipline$', views.DisciplineCreateView.as_view(),
         name='discipline_create'),
-    url(r'^(?P<pk>\d+)/detail-discipline$', DisciplineDetailView.as_view(),
+    url(r'^(?P<pk>\d+)/detail-discipline$', views.DisciplineDetailView.as_view(),
         name='discipline_detail'),
-    url(r'^(?P<period>\d+)/attendance-assign$', AttendanceAssign.as_view(),
+    url(r'^(?P<period>\d+)/attendance-assign$', views.AttendanceAssign.as_view(),
         name='attendance_assign'),
 )
