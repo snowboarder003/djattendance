82486e77992f748e9784322aa07a3831c6e4945b
Andree Kurniawan <lifeunion@gmail.com>
2014-07-23 09:16:42 -0700

    using runtests.py to launch tests instead of local.py settings

--- ap/runtests.py
+++ ap/runtests.py
@@ -8,14 +8,15 @@ settingsPath = "ap.settings.local"
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", settingsPath)
 from django.conf import settings
 from django_nose import NoseTestSuiteRunner
+from optparse import OptionParser
 
-def run_tests(*test_args):
+def run_tests(*test_args, **kwargs):
+    settings.NOSE_ARGS = ['--cover-package=absent_trainee_roster,accounts,attendance,books,classes,dailybread,hospitality,houses,leaveslips,lifestudies,localities,meal_seating,rooms,schedules,services,shortterm,syllabus,teams,terms,verse_parse','--cover-html']
     if not test_args:
         test_args = ['absent_trainee_roster','accounts','attendance','books','classes','dailybread','hospitality','houses','leaveslips','lifestudies','localities','meal_seating','rooms','schedules','services','shortterm','syllabus','teams','terms','verse_parse']
 
     # Run tests
     test_runner = NoseTestSuiteRunner(verbosity=1)
-
     failures = test_runner.run_tests(test_args)
 
     if failures:
