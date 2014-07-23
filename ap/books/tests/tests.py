d2bff02b1626c0802377549d1bca19d35d0075a1
Andree Kurniawan <lifeunion@gmail.com>
2014-07-23 15:04:00 -0700

    fix adding djangorestframework to requirements

--- ap/books/tests/tests.py
+++ ap/books/tests/tests.py
@@ -10,7 +10,7 @@ Pay attention to:
 4. how to retrieve created data globally from data generator outside the class
 And don't forget to start your test method with "test_"!!!
 
-Note: this unit-test is ONLY considered complete when coverage for "classes/models" = 100%
+Note: this unit-test is ONLY considered complete when coverage for "books/models" = 100%
 """
 
 #from django.test import TestCase
