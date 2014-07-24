ad6997b8e5e876db7a3a1740c4cc5e788a269765
Dennis Lu <ludennis@hotmail.com>
2014-07-24 14:30:39 -0700

    added a table in 'House Management'

--- ap/houses/urls.py
+++ ap/houses/urls.py
@@ -1,7 +1,9 @@
 from django.conf.urls import patterns, url
-from .views import HouseListView
+from .views import HouseListView, BunkListView
 
 urlpatterns = patterns(
 	'',
-	url(r'^$', HouseListView.as_view(), name='list'),
+	url(r'^$', HouseListView.as_view(), name='house_list'),
+	url(r'^bunk$', BunkListView.as_view(), name='bunk_list'),
+
 )
