cdb8bac7f1184ad6a9640cd7c9501d8c2a3df1a8
Jonathan Tien <jonathan.tien@gmail.com>
2014-07-25 18:11:59 -0700

    dynamically size seating chart template using react

--- ap/seating/views.py
+++ ap/seating/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from django.views.generic import TemplateView
 
-# Create your views here.
+from .models import Template, Chart
+
+
+class TemplateCreate(TemplateView):
+    template_name = "seating/template_create.html" 
