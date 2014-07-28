7ea2128b533cd53f4b92f865243df7da705fcc70
Sarah Rumbley <srumbley@mit.edu>
2014-07-28 11:41:26 -0700

    moved jquery from static/jquery/ to static/js/. updated jquery from 1.10.1 to 1.11.1.

--- ap/ap/settings/local.py
+++ ap/ap/settings/local.py
@@ -10,9 +10,9 @@ INSTALLED_APPS += ('django_extensions',
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'djattendance',
+        'NAME': 'ap',
         'USER': 'ap',
-        'PASSWORD': '4livingcreatures',
+        'PASSWORD': '',
         'HOST': 'localhost',  # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
         'PORT': '',  # Set to empty string for default.
     }
