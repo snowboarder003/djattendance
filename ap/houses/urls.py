c28b78c4f5c0b295a2ae30f5a3193da4d5683b70
Dennis Lu <ludennis@hotmail.com>
2014-07-30 15:14:33 -0700

    deleted 'static/houses' & 'templates/houses'

--- ap/houses/urls.py
+++ ap/houses/urls.py
@@ -1,9 +1,8 @@
 from django.conf.urls import patterns, url
-from .views import HouseListView, BunkListView
+from .views import BunkListView
 
 urlpatterns = patterns(
 	'',
-	url(r'^$', HouseListView.as_view(), name='house_list'),
-	url(r'^bunk$', BunkListView.as_view(), name='bunk_list'),
+	url(r'^bunks$', BunkListView.as_view(), name='bunk_list'),
 
 )
--- ap/houses/views.py
+++ ap/houses/views.py
@@ -1,10 +1,5 @@
 from django.views.generic.list import ListView
-from .models import House, Bunk
-
-class HouseListView(ListView):
-	model = House
-	context_object_name = 'houses'
-	template_name = 'houses/house_list.html'
+from .models import Bunk
 
 
 class BunkListView(ListView):
