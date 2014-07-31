4d19a693e089850c6ac4c7d0b79e6ecb9051e609
Dennis Lu <ludennis@hotmail.com>
2014-07-30 17:23:54 -0700

    bunk assignment complete

--- ap/houses/views.py
+++ ap/houses/views.py
@@ -23,10 +23,32 @@ class BunkFormView(FormView):
 
 	def form_valid(self, form):
 		if form.is_valid():
-			if form.cleaned_data['trainee'] == None:
-				Bunk.objects.get(pk=self.kwargs['pk']).trainee.bunk = None
-			else:
-				trainee = form.cleaned_data['trainee']
-				trainee.bunk = Bunk.objects.get(pk=self.kwargs['pk'])
-				trainee.save()
+			bunk = Bunk.objects.get(pk=self.kwargs['pk'])
+			new_occupant = form.cleaned_data['trainee']
+
+			# if bunk were to be assigned to 'vacant'
+			if new_occupant == None:
+				if hasattr(bunk, 'trainee'):
+					old_occupant = bunk.trainee
+					old_occupant.bunk = None
+					old_occupant.save()
+			# if bunk is vacant	
+			elif not hasattr(bunk, 'trainee'):
+				new_occupant.bunk = bunk
+				new_occupant.save()
+			# if new_occupant is to replace the old occupant
+			elif hasattr(bunk, 'trainee') and new_occupant.bunk == None:
+				old_occupant = bunk.trainee
+				old_occupant.bunk = None
+				new_occupant.bunk = bunk
+				old_occupant.save()
+				new_occupant.save()
+			# if new_occupant were to swap with old occupant
+			elif hasattr(bunk, 'trainee') and new_occupant.bunk != None:
+				old_occupant = bunk.trainee
+				old_occupant.bunk = new_occupant.bunk
+				new_occupant.bunk = bunk
+				old_occupant.save()
+				new_occupant.save()
+
 		return super(BunkFormView, self).form_valid(form)
