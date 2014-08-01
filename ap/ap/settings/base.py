1fc6534fa6b17d6ee3ce258f682bc22b476dfa82
Jonathan Tien <jonathan.tien@gmail.com>
2014-08-01 16:14:26 -0700

    moved bootstrap static files and updated refs

--- ap/ap/settings/base.py
+++ ap/ap/settings/base.py
@@ -214,10 +214,10 @@ LOGGING = {
 
 BOOTSTRAP3 = {
     'jquery_url': '/static/js/jquery-1.11.1.min.js',
-    'base_url': '/static/bootstrap/css/',
-    'css_url': None,
-    'theme_url': None,
-    'javascript_url': None,
+    'base_url': None,
+    'css_url': '/static/css/bootstrap.min.css',
+    'theme_url': '/static/css/bootstrap-theme.min.css',
+    'javascript_url': '/static/js/bootstrap.min.js',
     'horizontal_label_class': 'col-md-2',
     'horizontal_field_class': 'col-md-4',
 }
