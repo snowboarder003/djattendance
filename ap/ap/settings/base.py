d3963383ca289ed5dd2fd25b40122602de71ef4e
Jonathan Tien <jonathan.tien@gmail.com>
2014-08-09 13:59:03 -0700

    removed grappelli template

--- ap/ap/settings/base.py
+++ ap/ap/settings/base.py
@@ -85,7 +85,6 @@ SECRET_KEY = 'h%)g$1=j)_(lozsexfe*=$iwj9l#8mfaszohyg5n0azz691r#b'
 TEMPLATE_CONTEXT_PROCESSORS = (
     "django.contrib.auth.context_processors.auth",
     "django.core.context_processors.debug",
-    "django.core.context_processors.i18n",
     "django.core.context_processors.media",
     "django.core.context_processors.request",
     "django.contrib.messages.context_processors.messages",
