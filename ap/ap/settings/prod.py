e83497fc893545b767871f703a86f29f034ddf65
Jonathan Tien <jonathan.tien@gmail.com>
2014-08-11 19:48:12 -0700

    modified bootstrap3 paths

--- ap/ap/settings/prod.py
+++ ap/ap/settings/prod.py
@@ -5,7 +5,7 @@ TEMPLATE_DEBUG = False
 
 # Parse database configuration from $DATABASE_URL
 import dj_database_url
-DATABASES = {'default' : dj_database_url.config(default='postgres://django:attend2god@localhost/ap')}
+DATABASES = {'default' : dj_database_url.config()}
 
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
@@ -22,3 +22,13 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'ap/static'),
 )
+
+BOOTSTRAP3 = {
+    'jquery_url': STATIC_ROOT + 'js/jquery-1.11.1.min.js',
+    'base_url': None,
+    'css_url': STATIC_ROOT + 'css/bootstrap.min.css',
+    'theme_url': None,
+    'javascript_url': STATIC_ROOT + 'js/bootstrap.min.js',
+    'horizontal_label_class': 'col-md-2',
+    'horizontal_field_class': 'col-md-4',
+}
