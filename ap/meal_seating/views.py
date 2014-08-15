650cd076b8cd7c960c11b134cdd25049ee9e313d
Jonathan Yao <jonyao.o@gmail.com>
2014-08-15 15:48:49 -0700

    updated dates to have matching format

--- ap/meal_seating/views.py
+++ ap/meal_seating/views.py
@@ -21,10 +21,10 @@ def newseats(request):
     return render(request, 'newseating.html')
 
 def signin(request):
-    trainees = User.objects.all().filter(is_active=1).order_by("lastname")[:50]
+    trainees = User.objects.all().filter(is_active=1).exclude(is_staff=1, is_admin=1).order_by("lastname")[:50]
     startdate = date.today()
     two_week_datelist = []
     for x in range(0,14):
         mydate = startdate + timedelta(days=x)
-        two_week_datelist.append(format(mydate))
+        two_week_datelist.append(mydate.strftime('%m/%d/%Y'))
     return render(request, 'mealsignin.html', {'trainees' : trainees, 'start_date' : startdate, "two_week_datelist" : two_week_datelist})
