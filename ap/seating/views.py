e86e90c67af0747cfb81bef1c762db556a2f1e64
Jonathan Tien <jonathan.tien@gmail.com>
2015-04-30 19:53:38 -0700

    halting dev on seating module for now

--- ap/seating/views.py
+++ ap/seating/views.py
@@ -1,7 +1,12 @@
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, DetailView
 
 from .models import Template, Chart
 
 
 class TemplateCreate(TemplateView):
-    template_name = "seating/template_create.html" 
+    template_name = "seating/template_create.html"
+
+
+class TemplateDetail(DetailView):
+    queryset = Template.objects.all()
+    template_name = "seating/template_detail.html"
